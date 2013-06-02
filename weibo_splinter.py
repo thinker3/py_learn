87ac625e920c18398748b6d2ee866c7884dce1e5
thinker3 <metathinkerk@gmail.com>
2013-06-02 14:25:06 +0800

    modified weibo_splinter.py

--- weibo_splinter.py
+++ weibo_splinter.py
@@ -1,5 +1,9 @@
+#coding=utf8
 from splinter import Browser
+import sys
 
+content = unicode(sys.argv[1], 'utf-8')
+print content
 url = 'http://weibo.com'
 
 browser = Browser('firefox', extensions=['firebug.xpi',])
@@ -9,5 +13,6 @@ browser.find_by_name('password').click()
 browser.fill('password','tui123')
 browser.find_by_xpath('/html/body/div/div[2]/div[2]/div[2]/div/div[6]/a').click()
 #browser.find_by_xpath('/html/body/div/div/div[3]/div/div[2]/div/div/div/div/div[2]/textarea').click()
-browser.fill('', 'python splinter auto send test')
+browser.fill('', content)
 browser.find_by_xpath('/html/body/div/div/div[3]/div/div[2]/div/div/div/div/div[3]/div/a').click()
+
