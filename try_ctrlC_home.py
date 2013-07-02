66edd182cc17fe801f2a072d339bccbc6154180f
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-02 17:48:52 +0800

    0702

--- try_ctrlC_home.py
+++ try_ctrlC_home.py
@@ -1,5 +1,7 @@
 from time import sleep
 import os, sys
+import atexit
+
 filepath = os.path.expanduser('~/num.txt')
 try:
     f = open(filepath, 'r')
@@ -9,16 +11,24 @@ try:
 except:
     num = 1
 
+def save(num):
+    f = open(filepath, 'w+')
+    f.write(str(num))
+    f.close()
+
 try:
     while 1:
         print 'hello %d' % num
         sleep(0.1)
         num += 1
+        a = 1/(num - 120)
+except KeyboardInterrupt:
+    pass # or raise it to higher handler
 except:
     print 'ctrl c'
-    f = open(filepath, 'w+')
-    f.write(str(num))
-    f.close()
-    sys.exit(0)
+    save(num)
+    os._exit(0) # This line here will prevent finally and atexit from executing
 finally:
     print 'finally'
+
+atexit.register(save, num)
