701f238d11073c871e47ebb7d897a333812aa1a9
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-02 15:53:01 +0800

    0702

--- try_ctrlC_home.py
+++ try_ctrlC_home.py
@@ -1,20 +1,24 @@
 from time import sleep
-import os
+import os, sys
 filepath = os.path.expanduser('~/num.txt')
-
-f = open(filepath, 'r')
-num = f.readline()
-num = int(num)
-f.close()
+try:
+    f = open(filepath, 'r')
+    num = f.readline()
+    num = int(num)
+    f.close()
+except:
+    num = 1
 
 try:
     while 1:
-        print 'hello'
+        print 'hello %d' % num
         sleep(0.1)
+        num += 1
 except:
-    pass
-finally:
     print 'ctrl c'
     f = open(filepath, 'w+')
-    f.write(str(num+1))
+    f.write(str(num))
     f.close()
+    sys.exit(0)
+finally:
+    print 'finally'
