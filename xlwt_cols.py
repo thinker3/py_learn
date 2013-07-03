1fd535a11f1a0f12a71e71d7cb5c19684fef6e5c
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-03 15:55:36 +0800

    good

--- xlwt_cols.py
+++ xlwt_cols.py
@@ -1,8 +1,9 @@
 #coding=utf8
 from xlwt import Workbook
 
-class Person():
-    pass
+class Person(object):
+    def __getattr__(self, name):
+        return object.__getattribute__(self, name)
 
 obj = Person()
 obj.name = 'chenkun'
@@ -10,8 +11,6 @@ obj.age = 27
 obj.height = 163
 obj.weight = 60
 
-book = Workbook()
-sheet = book.add_sheet('columns')
 
 columns = [
         ['name', u'姓名', 8],
@@ -19,18 +18,38 @@ columns = [
         ['height', u'身高', 6],
         ['weight', u'体重', 6],
     ]
-def index_of(key, list_2d):
-    for i, one in enumerate(list_2d):
-        if key == one[0]:
-            return i
 
-for i in xrange(len(columns)):
-    sheet.write(0, i, columns[i][1]) 
-    sheet.col(i).width = columns[i][2] * 256
-for i in xrange(1, 10):
-    sheet.write(i, index_of('name', columns), obj.name)
-    sheet.write(i, index_of('age', columns), obj.age)
-    sheet.write(i, index_of('height', columns), obj.height)
-    sheet.write(i, index_of('weight', columns), obj.weight)
+objs = []
+for i in xrange(33):
+    objs.append(obj)
 
-book.save('Columns.xls')
+
+def make_xls(sheetname='sheet_1', filename='filename.xls', columns=[], objs=[]):
+    book = Workbook()
+    sheet = book.add_sheet(sheetname)
+
+    def index_of(key, list_2d):
+        for i, one in enumerate(list_2d):
+            if key == one[0]:
+                return i
+
+    attrs = []
+    for inner_list in columns:
+        attrs.append(inner_list[0])
+
+
+    for i in xrange(len(columns)):
+        sheet.write(0, i, columns[i][1]) 
+        sheet.col(i).width = columns[i][2] * 256
+    for i, obj in enumerate(objs, start=1):
+        for attr in attrs:
+            sheet.write(i, index_of(attr, columns), obj.__getattribute__(attr))
+
+    book.save(filename)
+
+if __name__ == '__main__':
+    make_xls(sheetname='test', filename='abc.xls', columns=columns, objs=objs)
+
+
+
+
