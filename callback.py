e16aaf152297bf3bf19ce2c37d6ee18a35e825e5
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-10 17:39:16 +0800

    callback | try scope

---
+++ callback.py
@@ -0,0 +1,13 @@
+from sys import argv
+def show(sth):
+    print 'simple callback function to show %s' % sth
+
+def main(num, callback=None, sth=None):
+    if num:
+        callback(sth)
+    else:
+        print 'callback function not called'
+
+if __name__ == '__main__':
+    num = int(argv[1])
+    main(num, show, 'abc')
---
+++ scope_try.py
@@ -0,0 +1,6 @@
+try:
+    a = 1 # a must be defined before possible exception
+    a = 30/0
+except:
+    if a:
+        print a
