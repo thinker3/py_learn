5524a2742d5fad4883e6c0df3ad1c059ae5ed33e
thinker3 <metathinkerk@gmail.com>
2013-07-10 23:14:08 +0800

    splinter to

--- splinter_html.py
+++ splinter_html.py
@@ -3,13 +3,14 @@ from splinter import Browser
 url = 'http://www.dzsc.com/stock-ic/AD51_025.html'
 firefox= Browser()
 firefox.visit(url)
-html = firefox.html
-print type(html)
-html = html.encode('gb18030')
-print type(html)
+firefox.
 
-f = open('_splinter.html', 'w')
-f.write(html)
-f.close()
 
-firefox.quit()
+#html = firefox.html
+#print type(html)
+#html = html.encode('gb18030')
+#print type(html)
+#f = open('_splinter.html', 'w')
+#f.write(html)
+#f.close()
+#firefox.quit()
