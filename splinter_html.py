23d971812649674c76e5818cd8f4f4ef6fcae17a
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-10 16:37:08 +0800

    splinter get html

---
+++ splinter_html.py
@@ -0,0 +1,15 @@
+from splinter import Browser
+
+url = 'http://www.dzsc.com/stock-ic/AD51_025.html'
+firefox= Browser()
+firefox.visit(url)
+html = firefox.html
+print type(html)
+html = html.encode('gb18030')
+print type(html)
+
+f = open('_splinter.html', 'w')
+f.write(html)
+f.close()
+
+firefox.quit()
