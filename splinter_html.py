9312ce23978be407ad59829afffb761ce7d355bb
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-11 09:34:30 +0800

    get html by js

--- splinter_html.py
+++ splinter_html.py
@@ -1,16 +1,28 @@
 from splinter import Browser
+import time
 
+def get_qqs_html(qqs):
+    html = qqs.html
+    if not html:
+        time.sleep(1)
+        return get_qqs_html(qqs)
+    else:
+        return html
 url = 'http://www.dzsc.com/stock-ic/AD51_025.html'
 firefox= Browser()
 firefox.visit(url)
-firefox.
+firefox.check('checkbox')
+qqs = firefox.find_by_id('qqmsn1')
+if qqs:
+    qqs = qqs[0]
+    print dir(qqs)
+    print
+    html = get_qqs_html(qqs)
+    print html
 
-
-#html = firefox.html
-#print type(html)
-#html = html.encode('gb18030')
-#print type(html)
-#f = open('_splinter.html', 'w')
-#f.write(html)
-#f.close()
-#firefox.quit()
+html = firefox.html
+html = html.encode('gb18030')
+f = open('_splinter.html', 'w')
+f.write(html)
+f.close()
+firefox.quit()
