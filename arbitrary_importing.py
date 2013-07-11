aec894ac77b82245081883d1f079f4201258033c
chenkun <chenkun@chenkun-P43T-ES3G.(none)>
2013-07-11 17:08:56 +0800

    python -m

--- arbitrary_importing.py
+++ arbitrary_importing.py
@@ -1,24 +1,38 @@
-import sys
-p = sys.path
-print p[0]
-p[0] = '/'.join(p[0].split('/')[:-1])
-print p[0]
+def main():
+    import sys
+    p = sys.path
+    print p[0]
+    p[0] = '/'.join(p[0].split('/')[:-1])
+    print p[0]
 
-from base.basic_functions import get_html
-print get_html
+    from base.basic_functions import get_html
+    print get_html
 
-p[0] = '/home/chenkun/work/'
-print p[0]
+    p[0] = '/home/chenkun/work/'
+    print p[0]
 
-from data_scrapy.mysql_tools import get_cur_conn
-print get_cur_conn
+    from data_scrapy.mysql_tools import get_cur_conn
+    print get_cur_conn
+
+    #p.append('/home/chenkun/git/weight/')
+    p.insert(0, '/home/chenkun/git/weight/')
+    for one in p:
+        if one.startswith('/h'):
+            print one
+
+    from write import init_db
+    print init_db 
 
 
-#p.append('/home/chenkun/git/weight/')
-p.insert(0, '/home/chenkun/git/weight/')
-for one in p:
-    if one.startswith('/h'):
-        print one
+def minor():
+    """
+    run in the upper folder:
+    python -m py_learn.arbitrary_importing
+    """
+    import sys
+    p = sys.path
+    print p[0]
+    from base.basic_functions import get_html
+    print get_html
 
-from write import init_db
-print init_db 
+minor()
