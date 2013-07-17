24bef095a7d7d2ddb0f051b5dabf218a71ab6556
thinker3 <metathinkerk@gmail.com>
2013-07-17 13:35:32 +0800

    charset

--- mysql_operate.py
+++ mysql_operate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+#coding=utf8
 
 import MySQLdb as mysql
 
@@ -8,16 +8,20 @@ DB_NAME = 'test'
 DB_USER = 'chenkun'
 DB_PASSWORD = 'ck1234'
 
-conn = mysql.connect(host=DB_HOST, db=DB_NAME, user=DB_USER, passwd=DB_PASSWORD,charset='utf8')
+conn = mysql.connect(host=DB_HOST, db=DB_NAME, user=DB_USER, passwd=DB_PASSWORD, charset='') # charset is the key of messed encoding
 cur = conn.cursor()
 #sql = 'alter table words add example varchar(255)'
 #sql = 'alter table words drop analysed_flag'
 #sql = "insert into words(id,name,phonetic,meaning,example) values(2,'world','wd','','hello world')"
 #sql = "insert into words(name,phonetic,meaning,example) values('world','wd','世界','hello world')"
 #sql = "update words set meaning='世界'"
-#sql = "update words set meaning='哈罗' where id<2"
-sql = "delete from words where id>2"
-r = cur.execute(sql)
+meaning = u'哈哈1'
+example = u'哈哈2'
+meaning = meaning.encode('utf8')
+example = example.encode('utf8')
+sql = "update words set meaning=%s, example=%s where id<2"
+#sql = "delete from words where id>2"
+r = cur.execute(sql, (meaning, example))
 conn.commit()
 cur.close()
 conn.close()
