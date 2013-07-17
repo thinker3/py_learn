3baff6e2e2bcba664c8707f81ee9ef6beaeb0290
thinker3 <metathinkerk@gmail.com>
2013-07-17 16:26:51 +0800

    string format with list does not work

--- mysql_operate.py
+++ mysql_operate.py
@@ -15,13 +15,13 @@ cur = conn.cursor()
 #sql = "insert into words(id,name,phonetic,meaning,example) values(2,'world','wd','','hello world')"
 #sql = "insert into words(name,phonetic,meaning,example) values('world','wd','世界','hello world')"
 #sql = "update words set meaning='世界'"
-meaning = u'哈哈1'
-example = u'哈哈2'
+meaning = u'哈哈3'
+example = u'哈哈4'
 meaning = meaning.encode('utf8')
 example = example.encode('utf8')
 sql = "update words set meaning=%s, example=%s where id<2"
 #sql = "delete from words where id>2"
-r = cur.execute(sql, (meaning, example))
+r = cur.execute(sql, [meaning, example])
 conn.commit()
 cur.close()
 conn.close()
