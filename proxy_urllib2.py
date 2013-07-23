ce1d09ddae47ae4367395c387fd688b496212450
thinker3 <metathinkerk@gmail.com>
2013-07-23 19:23:48 +0800

    both https

--- proxy_urllib2.py
+++ proxy_urllib2.py
@@ -1,10 +1,13 @@
 import urllib, urllib2, simplejson as json
-url = 'https://twitter.com'
-proxy = urllib2.ProxyHandler({'https': '127.0.0.1:8088'})
-opener = urllib2.build_opener(proxy)
-urllib2.install_opener(opener)
-html = urllib2.urlopen(url).read()
-f= open('twitter.html', 'w')
-f.write(html)
-f.close()
 
+def main(a, b):
+    url = a + '://twitter.com'
+    proxy = urllib2.ProxyHandler({b: '127.0.0.1:8088'})
+    opener = urllib2.build_opener(proxy)
+    urllib2.install_opener(opener)
+    html = urllib2.urlopen(url).read()
+    f= open('twitter.html', 'w')
+    f.write(html)
+    f.close()
+
+main('https', 'https') # both should be https
