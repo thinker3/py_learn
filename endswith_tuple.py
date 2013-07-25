83624c464dae85e3cabddea0e8b01a40505e1675
thinker3 <metathinkerk@gmail.com>
2013-07-25 22:26:22 +0800

    endswith tuple

---
+++ endswith_tuple.py
@@ -0,0 +1,9 @@
+patterns = ['.appspot.com', '.google.com', '.googleusercontent.com', 
+            '.googleapis.com', 'googlecode.com', '.gstatic.com', '.google.com.hk']
+patterns = tuple(patterns)
+
+host1 = 'code.google.com'
+host2 = 'www.baidu.com'
+
+print host1.endswith(patterns)
+print host2.endswith(patterns)
