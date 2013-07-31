a5047fb8995300506fed3aff29be9358e5bb2918
thinker3 <metathinkerk@gmail.com>
2013-07-31 09:18:56 +0800

    os path exists, os makedirs

--- os_path.py
+++ os_path.py
@@ -9,3 +9,9 @@ print os.getcwd()
 print os.walk(os.getcwd())
 for one in os.walk(os.getcwd()):
     print one
+
+dirname = os.path.dirname(__file__)
+test = os.path.join(dirname, 'test/')
+if not os.path.exists(test):
+    os.makedirs(test)
+
