0c7cda43712871b4a22fc052547271a5374c2927
thinker3 <metathinkerk@gmail.com>
2013-08-01 04:35:24 +0800

    sample, choice

--- rand.py
+++ rand.py
@@ -1,6 +1,14 @@
 import random
 n=0
-while n<100:
-    r = random.randint(0,5)
+while n<10:
+    r = random.randint(0,10) # integers in [0, 10]
     print r,
     n += 1
+
+print
+numbers = range(0, 20)
+some_numbers= random.sample(numbers, 5)
+print numbers
+print some_numbers
+
+print random.choice(numbers)
