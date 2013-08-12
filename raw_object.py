2a153beff2a66c03b858417f5bb645048c666745
thinker3 <metathinkerk@gmail.com>
2013-08-12 09:16:40 +0800

    set obj attr directly

---
+++ raw_object.py
@@ -0,0 +1,17 @@
+#student = object()
+#student.name = 'Jack'
+#print student.name
+
+
+
+class Person(object):
+    pass
+class Address(object):
+    pass
+person = Person()
+person.name = 'Tom'
+person.address = Address()
+person.address.zip = '40015'
+
+print person.name
+print person.address.zip
