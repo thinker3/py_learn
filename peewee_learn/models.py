869b2bac74d22355cde5d9c797b6c4368f5a1254
thinker3 <metathinkerk@gmail.com>
2013-08-26 10:35:00 +0800

    peewee learn

---
+++ peewee_learn/models.py
@@ -0,0 +1,18 @@
+#coding=utf8
+import peewee
+
+db = peewee.SqliteDatabase('school.db')
+
+class Teacher(peewee.Model):
+    name = peewee.CharField()
+    class Meta:
+        database = db
+
+class Student(peewee.Model):
+    name = peewee.CharField()
+    teacher = peewee.ForeignKeyField(Teacher, related_name='students')
+    class Meta:
+        database = db
+
+
+
