2acb443ec61ec8260eef11609ec0a35f152ddac6
thinker3 <metathinkerk@gmail.com>
2013-08-31 22:28:19 +0800

    after

--- gui_learn/deiconify.py
+++ gui_learn/deiconify.py
@@ -1,22 +1,15 @@
-#coding=utf8
 from time import sleep
-from Tkinter import Tk, Toplevel
-import Tkinter
+from Tkinter import Tk
 
 class GUI():
     def __init__(self):
         self.root = Tk()
-        self.root.title("Test")
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        self.root.geometry("550x250+%d+%d" % (screen_width/2-275, screen_height/2-125))
         self.root.protocol("WM_DELETE_WINDOW", self.close_handler)
         self.root.mainloop()
 
     def close_handler(self):
         self.root.iconify()
-        self.root.call('wm', 'deiconify', '.')
-
+        self.root.after(100, self.root.deiconify())
 
 if __name__ == '__main__':
     gui = GUI()
