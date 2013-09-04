cdfdc1b403770520e0604bd83f1fa2a0d0521cc8
thinker3 <metathinkerk@gmail.com>
2013-09-04 17:43:07 +0800

    pyqt signal

---
+++ gui_learn/pyqt_signal_slot.py
@@ -0,0 +1,22 @@
+import sys
+from time import sleep
+from PyQt4.QtCore import *
+ 
+class A(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+ 
+    def afunc (self):
+        self.emit(SIGNAL("go"))    
+ 
+    def bfunc(self):
+        print "Hello World!"
+ 
+if __name__=="__main__":
+    app=QCoreApplication(sys.argv)
+    a=A()
+    QObject.connect(a,SIGNAL("go"),a.bfunc)
+    while 1:
+        a.afunc()    
+        sleep(1)
+    sys.exit(app.exec_())
