118caa82b1249c236cb21b688750bfc176f53ac8
thinker3 <metathinkerk@gmail.com>
2013-09-05 21:25:13 +0800

    bs4 no good for cdata

---
+++ bs4_cdata.py
@@ -0,0 +1,19 @@
+#coding=utf8
+from bs4 import CData
+from bs4 import BeautifulSoup
+from bs4.builder import LXMLTreeBuilderForXML
+
+xml = \
+'''
+<?xml version="1.0" ?>
+<foo>
+    <bar><![CDATA[!@#$%^&*()_+{}|:"<>?,./;'[]\-=]]></bar>
+</foo>
+'''
+builder=LXMLTreeBuilderForXML()
+soup = BeautifulSoup(xml, "xml") 
+print(soup.new_string)
+soup.foo.bar.string = CData(soup.foo.bar.string)
+soup = soup.prettify(formatter="xml")
+print(soup)
+
