7bbf582f1c42a088480657bc94700d4b50172bbb
thinker3 <metathinkerk@gmail.com>
2013-09-08 00:56:50 +0800

    splinter js

--- splinter_js.py
+++ splinter_js.py
@@ -1,3 +1,6 @@
 from splinter.browser import Browser
 b = Browser('firefox')
 b.visit('http://www.baidu.com')
+b.fill('wd', 'test')
+b.execute_script('document.getElementsByName("f")[0].submit()')
+
