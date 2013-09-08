d51cd96b583c63b597bafd0221600ec6ca0929b0
thinker3 <metathinkerk@gmail.com>
2013-09-08 08:57:52 +0800

    signal non blocking

---
+++ signal_non_blocking.py
@@ -0,0 +1,22 @@
+import signal
+
+class AlarmException(Exception):
+    pass
+
+def alarmHandler(signum, frame):
+    raise AlarmException
+
+def nonBlockingRawInput(prompt='', timeout=5):
+    signal.signal(signal.SIGALRM, alarmHandler)
+    signal.alarm(timeout)
+    try:
+        text = raw_input(prompt)
+        signal.alarm(0)
+        return text
+    except AlarmException:
+        print '\nPrompt timeout. Continuing...'
+    signal.signal(signal.SIGALRM, signal.SIG_IGN)
+    return ''
+
+print nonBlockingRawInput()
+print 'hello'
