b095dc7fdc0f5abed91ab4ab2fda3958f3046921
thinker3 <metathinkerk@gmail.com>
2013-09-13 02:23:51 +0800

    delta format print

--- date_format.py
+++ date_format.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from time import sleep
 
 s = '20130824 09:32:12'
 time = datetime.strptime(s, '%Y%m%d %H:%M:%S')
@@ -8,3 +9,10 @@ print type(time)
 s = datetime.strftime(time, '%Y/%m/%d %H:%M:%S')
 print s
 print type(s)
+
+before = datetime.now()
+sleep(1)
+now = datetime.now()
+delta = now - before
+print '%s seconds' % delta.total_seconds()
+print '%f seconds' % delta.total_seconds()
