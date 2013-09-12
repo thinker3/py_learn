14026be757c7707551fa24aed9e021788e62bb5b
thinker3 <metathinkerk@gmail.com>
2013-09-13 02:14:09 +0800

    time delta

--- date_diff.py
+++ date_diff.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta, datetime
+from time import sleep
 
 
 
@@ -12,4 +13,14 @@ print abs(day_delta)
 
 print type(today.year)
 
-print datetime.now() # fit for django datetime field
+before = datetime.now() # fit for django datetime field
+sleep(2)
+now = datetime.now()
+delta = now - before
+print now, type(now)
+print delta, type(delta)
+print dir(delta)
+print delta.total_seconds(), type(delta.total_seconds())
+print delta.days, type(delta.days)
+print delta.seconds, type(delta.seconds)
+print delta.microseconds, type(delta.microseconds)
