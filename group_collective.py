cb5804f85e5ed330560364d0b3636b4e41f4ba96
thinker3 <metathinkerk@gmail.com>
2013-09-14 06:27:53 +0800

    groups to split

--- group_collective.py
+++ group_collective.py
@@ -6,8 +6,21 @@ s = "February 2011"
 ss = "Feb 2011"
 sss = "Febr 2011"
 
-m = re.match(p,sss)
-s
+m = re.match(p, ss)
 print m
 print m.group()
 print m.groups()
+
+
+s_list = [
+    'something{now I am wrapped {I should {not{} cause} splitting} I am still wrapped} something else',
+    'something{now I am wrapped {I should {not{} cause} splitting} I am still wrapped}',
+    '{now I am wrapped {I should {not{} cause} splitting} I am still wrapped} something else',
+    '{now I am wrapped {I should {not{} cause} splitting} I am still wrapped}',
+    'now I am wrapped {I should {not{} cause} splitting} I am still wrapped}',
+    '{now I am wrapped {I should {not{} cause} splitting} I am still wrapped',
+    'now I am wrapped {I should {not{} cause} splitting} I am still wrapped',
+    ]
+for one in s_list:
+    m = re.search(r'([^{]*)({)(.*)(})([^}]*)', one)
+    print m.groups()
