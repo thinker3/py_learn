1c7361be2380ce9db4590569b5ac2533236ef632
thinker3 <metathinkerk@gmail.com>
2013-09-16 06:27:11 +0800

    about not contain some attr

--- not_contain.py
+++ not_contain.py
@@ -4,19 +4,22 @@ text = '''\
 <table>
   <tbody>
     <tr>
-      <td>
-        <strong>Specifications</strong>
-      </td>
+      <td>Black</td>
+      <td><strong>Specifications</strong></td>
     </tr>
+
     <tr>
       <td>Finish Specifications</td>
       <td>Black</td>
     </tr>
+
     <tr>
+      Specifications
       <td>Action</td>
       <td>Semi-Automatic</td>
     </tr>
-    <tr>
+
+    <tr a='Specifications'>
       <td>Caliber</td>
       <td>7.62mmX39mm</td>
     </tr>
@@ -24,9 +27,8 @@ text = '''\
 </table>'''
 
 hxs = HtmlXPathSelector(text=text)
-#trs = hxs.select('//table/tbody/tr[not(contains(td, "Specifications"))]')
-#trs = hxs.select('//table/tbody/tr[not(contains(., "Specifications"))]')
-#trs = hxs.select('//table/tbody/tr[not(contains(./td/strong, "Specifications"))]')
-trs = hxs.select('//table/tbody/tr[not(.//text()[contains(., "Specifications")])]')
+#trs = hxs.select('//table/tbody/tr[not(contains(td, "Specifications"))]') # wrong
+#trs = hxs.select('//table/tbody/tr[not(contains(.|./@*, "Specifications"))]') # wrong
+trs = hxs.select('//table/tbody/tr[not(.//text()|./@*[contains(., "Specifications")])]')
 for one in trs.extract():
     print one
