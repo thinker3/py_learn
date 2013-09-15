28b3bcd1ebe3c220578031b48a63dd61e39c4ee5
thinker3 <metathinkerk@gmail.com>
2013-09-15 12:30:21 +0800

    stdou to var

---
+++ stdout_to_var.py
@@ -0,0 +1,11 @@
+import subprocess
+p = subprocess.Popen("git st", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+p_stdout = p.stdout.read()
+p_stderr = p.stderr.read()
+
+print '*' * 30
+print p_stdout
+print '*' * 30
+print p_stderr
+print '*' * 30
