0a7c365024349522d7a046112ce4c0682b13fc7c
thinker3 <metathinkerk@gmail.com>
2013-09-17 20:25:34 +0800

    ignore namespace

--- xml_parse.py
+++ xml_parse.py
@@ -10,11 +10,41 @@ xml = \
 """
 
 
-xxs =XmlXPathSelector(text=xml)
+xxs = XmlXPathSelector(text=xml)
 foos = xxs.select('//foos')
 for one in foos:
     text = one.select('./foo//text()').extract()
     text = ''.join(text)
     print text
+
+xml = \
+"""
+    <content type="text/xml">
+      <s:dict>
+        <s:key name="group_id">MAC</s:key>
+        <s:key name="label">NOT FOR RESALE</s:key>
+        <s:key name="max_violations">5</s:key>
+        <s:key name="quota">1000000000</s:key>
+        <s:key name="relative_expiration_interval">0</s:key>
+        <s:key name="relative_expiration_start">0</s:key>
+        <s:key name="sourcetypes"><s:list/></s:key>
+        <s:key name="stack_id">mac</s:key>
+        <s:key name="status">VALID</s:key>
+        <s:key name="type">mac</s:key>
+        <s:key name="window_period">30</s:key>
+      </s:dict>
+    </content>
+"""
+
+xxs = XmlXPathSelector(text=xml)
+quota = xxs.select('//*[@name="quota"]/text()').extract()[0]
+print quota
+
+
+
+
+
+
+
 
 
