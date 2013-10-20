806f58c79770d0a5cde12e677a8da448fdbe9541
thinker3 <metathinkerk@gmail.com>
2013-10-21 00:17:17 +0800

    static class methods, class instance variables

--- class_static_method.py
+++ class_static_method.py
@@ -24,3 +24,85 @@ p.show()
 Person.show()
 p.say_hello()
 Person.say_hello()
+
+class Teacher():
+    def say_hello():
+        print 'Teacher'
+
+print Teacher.say_hello
+# unbound method say_hello() must be called with Teacher instance as first argument (got nothing instead)
+#Teacher.say_hello()
+print Teacher().say_hello
+# say_hello() takes no arguments (1 given)
+#Teacher().say_hello()
+
+class Teacher():
+    @staticmethod
+    def say_hello():
+        print 'Teacher'
+
+print Teacher.say_hello
+Teacher.say_hello()
+print Teacher().say_hello
+Teacher().say_hello()
+
+class Pro(Teacher):
+    pass
+
+print Pro.say_hello
+Pro.say_hello()
+print Pro().say_hello
+Pro().say_hello()
+
+class Pro(Teacher):
+    @staticmethod
+    def say_hello():
+        print 'Pro'
+
+print Pro.say_hello
+Pro.say_hello()
+print Pro().say_hello
+Pro().say_hello()
+
+class Teacher():
+    @staticmethod
+    def say_hello(what):
+        print 'Teacher'
+
+print Teacher.say_hello
+# say_hello() takes exactly 1 argument (0 given)
+#Teacher.say_hello()
+print Teacher().say_hello
+# say_hello() takes exactly 1 argument (0 given)
+#Teacher().say_hello()
+
+class Teacher():
+    @classmethod
+    def say_hello():
+        print 'Teacher'
+
+print Teacher.say_hello
+# say_hello() takes no arguments (1 given)
+#Teacher.say_hello()
+print Teacher().say_hello
+# say_hello() takes no arguments (1 given)
+#Teacher().say_hello()
+
+class Teacher():
+    @classmethod
+    def say_hello(what):
+        print 'Teacher'
+
+print Teacher.say_hello
+Teacher.say_hello()
+print Teacher().say_hello
+Teacher().say_hello()
+
+class Pro(Teacher):
+    pass
+
+print Pro.say_hello
+Pro.say_hello()
+print Pro().say_hello
+Pro().say_hello()
+
--- get_obj_by_method.py
+++ get_obj_by_method.py
@@ -8,6 +8,18 @@ class Pizza(object):
     def set_size(self, size):
         self.size = size
 
+    @staticmethod
+    def mix(x, y):
+        return x + y
+
+print Pizza.set_size == Pizza.set_size
+print Pizza.set_size is Pizza.set_size
+print Pizza(1) == Pizza(1)
+obj = Pizza(1)
+print obj.set_size == obj.set_size
+print obj.set_size is obj.set_size
+
+print '-' * 30
 m = Pizza(1).get_size
 obj = m.__self__
 print m == Pizza.get_size
@@ -18,5 +30,66 @@ print m is obj.get_size
 print obj.get_size()
 obj.set_size(2)
 print obj.get_size()
+
+print '-' * 30
+class Pizza(object):
+    @staticmethod
+    def mix(x, y):
+        return x + y
+
+print Pizza.mix is Pizza.mix
+print Pizza().mix is Pizza.mix
+print Pizza().mix is Pizza().mix
+
+class NewPizza(Pizza):
+    @staticmethod
+    def mix(x, y):
+        return 2*x + y
+
+pizza = Pizza()
+print pizza.mix(2,1)
+
+new_pizza = NewPizza()
+print new_pizza.mix(2,1)
+
+
+print '-' * 30
+class Pizza(object):
+    radius = 42
+    @classmethod
+    def get_radius(cls):
+        return cls.radius
+
+    def set_radius(self, radius):
+        self.radius = radius
+
+    def show(self):
+        return self.radius
+
+p = Pizza()
+print Pizza.get_radius()
+print Pizza.radius
+print p.get_radius()
+print p.show()
+
+p.set_radius(40)
+print Pizza.get_radius()
+print Pizza.radius
+print p.get_radius()
+print p.show()
+
+Pizza.radius = 30
+print Pizza.get_radius()
+print Pizza.radius
+print p.get_radius()
+print p.show()
+
+Pizza.size = 1
+print Pizza.size
+print p.size
+p.weight = 22
+print p.weight
+print Pizza.weight
+
 
 
