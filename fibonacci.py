1f76cfc3e9e1494763918d6483da374a27699617
thinker3 <metathinkerk@gmail.com>
2013-10-20 18:49:20 +0800

    simpler

--- fibonacci.py
+++ fibonacci.py
@@ -31,3 +31,17 @@ t = fibonacci_list(9)
 print type(t)
 for one in t:
     print one
+
+
+def fibonacci(n):
+    temp = [1,1]
+    i=1
+    while i<=n-2:
+        temp.append(temp[i-1]+temp[i])
+        i += 1
+    return temp
+
+print
+t = fibonacci(9)
+for one in t:
+    print one
