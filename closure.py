d80a0677b2c4aba50d420aad713e92a8bad0fc1b
thinker3 <metathinkerk@gmail.com>
2013-10-22 15:11:28 +0800

    closure late binding ?

--- closure.py
+++ closure.py
@@ -10,7 +10,7 @@ setter(1)
 print adder(5)
 
 
-
+print '-' * 30
 def make_adder_and_setter(x):
     x = [x]
     def setter(n):
@@ -21,3 +21,35 @@ adder, setter = make_adder_and_setter(10)
 print adder(5)
 setter(1)
 print adder(5)
+
+
+print '-' * 30
+def test():
+    for i in range(3):
+        def a():
+            print i
+            print locals()
+        a()
+        yield a
+    print locals()
+
+a, b, c = test()
+print '-' * 30
+a()
+b()
+c()
+
+
+print '-' * 30
+def test():
+    def func(i):
+            def a():
+                print i
+            return a
+    for i in range(3):
+        yield func(i)
+
+a, b, c = test()
+a()
+b()
+c()
