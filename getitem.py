3b427ef557d0d26c7b5467f5ada5505e0554946e
thinker3 <metathinkerk@gmail.com>
2013-10-24 10:59:49 +0800

    sequence and slice object

--- getitem.py
+++ getitem.py
@@ -18,3 +18,20 @@ except:
 
 
 print '-' * 30
+l = ['alpha', 'beta', 'gamma']
+print l.__getitem__(0)
+print l.__getitem__(-1)
+print l[1]
+print l[1:]
+print l.__getitem__(slice(1,None))
+print l.__getitem__(slice(1,))
+print l.__getitem__(slice(None,1))
+print l.__getitem__(slice(None,None))
+print l.__getitem__(slice(None,None,2))
+
+
+print '-' * 30
+t = ('alpha', 'beta')
+print t.__getitem__(0)
+print t.__getitem__(-1)
+print t[1]
