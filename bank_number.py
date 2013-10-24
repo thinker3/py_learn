9481b428d79240bf4b69ab8265fe9e0efe5311d3
thinker3 <metathinkerk@gmail.com>
2013-10-24 14:49:02 +0800

    nothing to do

---
+++ bank_number.py
@@ -0,0 +1,18 @@
+def new_number(a):
+    for i, l in enumerate(a, start=1):
+        if i%4 == 0:
+            yield l
+            if i != len(a):
+                yield ' '
+        else:
+            yield l
+
+a = '983497829489328492'
+a = list(new_number(a))
+print a
+print ''.join(a)
+
+a = '98349782948932849229'
+a = list(new_number(a))
+print a
+print ''.join(a)
