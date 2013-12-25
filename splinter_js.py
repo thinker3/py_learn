a7d097b6b7911cbc4892625d60e74f052a4c0dfe
thinker3 <metathinkerk@gmail.com>
2013-12-25 17:16:32 +0800

    js simulate enter key

--- splinter_js.py
+++ splinter_js.py
@@ -1,6 +1,17 @@
 from splinter.browser import Browser
 b = Browser('firefox')
 b.visit('http://www.baidu.com')
+#b.fill('wd', 'test\r')
+#b.fill('wd', 'test\n')
+#b.fill('wd', 'test\r\n')
+
 b.fill('wd', 'test')
-b.execute_script('document.getElementsByName("f")[0].submit()')
+#b.execute_script('document.getElementsByName("f")[0].submit()')
+
+script = '''
+    var e = document.createEvent('KeyboardEvent');
+    e.initKeyEvent('keydown', true, true, window, false, false, false, false, 13, 0);
+    document.getElementsByName('wd')[0].dispatchEvent(e)
+'''
+b.execute_script(script)
 
