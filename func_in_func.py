ce688829eb411042426d422eb1b873182dc05813
thinker3 <metathinkerk@gmail.com>
2014-01-20 14:19:11 +0800

    can not change value in inner func

---
+++ func_in_func.py
@@ -0,0 +1,29 @@
+def outer(a):
+    def inner():
+        print a, b
+        #a = 0 # UnboundLocalError: local variable 'a' referenced before assignment
+    b = 2
+    inner()
+
+outer(1)
+print '-' * 20
+
+
+def outer(a):
+    def inner():
+        #print a, b # NameError: global name 'a' is not defined
+        global a, b
+        a = 'a'
+        b = 'b'
+    b = 2
+    inner()
+    print a, b
+
+outer(1)
+print a, b
+
+
+
+
+
+
