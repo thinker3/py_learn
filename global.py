f50040e507e90a62ab677dcab3d0393a935a5255
thinker3 <metathinkerk@gmail.com>
2014-01-20 11:47:31 +0800

    something is done before calling

--- global.py
+++ global.py
@@ -15,3 +15,18 @@ total = 0
 for i in xrange(10):
     total += i+1
 print total
+print '-' * 20
+
+
+def test():
+    print total # 'total' is made local before calling test()???
+    total = 0
+
+import traceback
+try:
+    test()
+except:
+    traceback.print_exc()
+
+print '-' * 20
+print total
