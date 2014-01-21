95d14a082e02c0093e43e92f7a6c2f3d2e8cc565
thinker3 <metathinkerk@gmail.com>
2014-01-21 14:47:19 +0800

    no def

--- square.py
+++ square.py
@@ -64,10 +64,81 @@ f = open(argv[1], 'r')
 for one in f.readlines():
     if one not in ['\n']:
         print is_square(*get_points(one))
+f.close()
+print
 
 
+import sys 
+cases = open(sys.argv[1], 'r')
+for case in cases:
+    if case != '\n':
+        a, b, c, d = map(eval, case.split(', '))
+        ab = b[0]-a[0], b[1]-a[1]
+        dc = c[0]-d[0], c[1]-d[1]
+        ac = c[0]-a[0], c[1]-a[1]
+        bc = c[0]-b[0], c[1]-b[1]
+        bd = d[0]-b[0], d[1]-b[1]
+
+        cd = d[0]-c[0], d[1]-c[1]
+        ad = d[0]-a[0], d[1]-a[1]
+        cb = b[0]-c[0], b[1]-c[1]
+        if ab == dc:
+            if ab[0]*bc[0] + ab[1]*bc[1] == 0:
+                if ac[0]*bd[0] + ac[1]*bd[1] == 0:
+                    print 'true'
+                    continue
+        if ab == cd:
+            if ab[0]*ac[0] + ab[1]*ac[1] == 0:
+                if ad[0]*bc[0] + ad[1]*bc[1] == 0:
+                    print 'true'
+                    continue
+        if ad == cb:
+            if ad[0]*ac[0] + ad[1]*ac[1] == 0:
+                if ab[0]*dc[0] + ab[1]*dc[1] == 0:
+                    print 'true'
+                    continue
+        print 'false'
+
+cases.close()
+print
 
 
+import sys, re 
+cases = open(sys.argv[1], 'r')
+for case in cases:
+    if case != '\n':
+        nums = map(int, re.findall(r'\d+', case))
+        points= []
+        for i in range(4):
+            points.append((nums[i*2], nums[i*2+1]))
+        a, b, c, d = points
+        ab = b[0]-a[0], b[1]-a[1]
+        dc = c[0]-d[0], c[1]-d[1]
+        ac = c[0]-a[0], c[1]-a[1]
+        bc = c[0]-b[0], c[1]-b[1]
+        bd = d[0]-b[0], d[1]-b[1]
+
+        cd = d[0]-c[0], d[1]-c[1]
+        ad = d[0]-a[0], d[1]-a[1]
+        cb = b[0]-c[0], b[1]-c[1]
+        if ab == dc:
+            if ab[0]*bc[0] + ab[1]*bc[1] == 0:
+                if ac[0]*bd[0] + ac[1]*bd[1] == 0:
+                    print 'true'
+                    continue
+        if ab == cd:
+            if ab[0]*ac[0] + ab[1]*ac[1] == 0:
+                if ad[0]*bc[0] + ad[1]*bc[1] == 0:
+                    print 'true'
+                    continue
+        if ad == cb:
+            if ad[0]*ac[0] + ad[1]*ac[1] == 0:
+                if ab[0]*dc[0] + ab[1]*dc[1] == 0:
+                    print 'true'
+                    continue
+        print 'false'
+
+cases.close()
 
 
 
