a903cea5827a08c478a9ba35e4847a3c8baa0d32
thinker3 <metathinkerk@gmail.com>
2014-01-22 12:35:20 +0800

    index from 0, yield

--- mines.py
+++ mines.py
@@ -1,30 +1,26 @@
 from sys import argv
 
 def get_mines(m, n, s):
-    mines = []
     for i in range(m*n):
-        x = i/n + 1
-        y = (i+1)%n
-        if not y:
-            y = n
+        x = i/n
+        y = i%n
         if s[i] == '*':
-            mines.append('*')
+            yield '*'
         else:
             total = 0
             for a in [x-1, x, x+1]:
                 for b in [y-1, y, y+1]:
-                    if 0<a<=m and 0<b<=n:
-                        if s[(a-1)*n + b - 1] == '*':
+                    if 0<=a<m and 0<=b<n:
+                        if s[a*n + b] == '*':
                             total += 1
-            mines.append(str(total))
-    return ''.join(mines)
+            yield str(total)
 
 f = open(argv[1], 'r')
 for one in f.readlines():
     if one not in ['\n']:
         mn, s = one.split(';')
         m, n = map(int, mn.split(','))
-        print get_mines(m, n, s)
+        print ''.join(get_mines(m, n, s))
 f.close()
 
 
