dc53bca41481396bc0afe816eb1f97cc939885f7
thinker3 <metathinkerk@gmail.com>
2014-01-23 00:27:22 +0800

    no readlines

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
 from sys import argv
 f = open(argv[1], 'r')
-for one in f.readlines():
-    if one not in ['\n']:
+for one in f:
+    if one != '\n':
         print ' '.join(one.split()[::-1])
 f.close()
