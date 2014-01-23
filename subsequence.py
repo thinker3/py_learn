0e9c24a1ac75ebebf76759f1094eff1d4035b783
thinker3 <metathinkerk@gmail.com>
2014-01-23 09:54:17 +0800

    special cases

--- subsequence.py
+++ subsequence.py
@@ -7,9 +7,7 @@ def multiply(a, b):
             if isinstance(i, list):
                 if i[-1] >= j:
                     continue
-                temp = i[:]
-                temp.append(j)
-                product.append(temp)
+                product.append(i+[j])
             else:
                 if i >= j:
                     continue
@@ -17,13 +15,18 @@ def multiply(a, b):
     return [product]
 
 def get_num(one):
-    one = one.strip()
     case, word = one.split(',')
-    if not (case or word):
+    if not (case and word):
         print 0
         return
-    if case == word:
-        print 1
+    if len(case) < len(word):
+        print 0
+        return
+    if len(case) == len(word):
+        if case == word:
+            print 1
+        else:
+            print 0
         return
     occurrences = []
     for ch in word:
@@ -31,16 +34,13 @@ def get_num(one):
         occurrences.append(indexes)
     while len(occurrences) >= 2:
         occurrences = multiply(occurrences[0], occurrences[1]) + occurrences[2:]
-    try:
-        print len(occurrences[0])
-    except:
-        print 0
+    print len(occurrences[0])
 
 
 f = open(argv[1], 'r')
 for one in f:
     if one != '\n':
-        get_num(one)
+        get_num(one[:-1])
 f.close()
 
 
