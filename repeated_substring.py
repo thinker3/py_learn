303a7e5a166ece513bc26c0a0dcdb714cafa580e
thinker3 <metathinkerk@gmail.com>
2014-01-25 00:11:41 +0800

    the first max repeated substring

---
+++ repeated_substring.py
@@ -0,0 +1,23 @@
+from sys import argv
+
+def max_repeated_substring(one):
+    one = one[:-1]
+    n = len(one)
+    ans = ''
+    for i in range(n/2, 0, -1):
+        for j in range(n-2*i, -1, -1):
+            a, b = one[j:j+i], one[j+i:]
+            if not a.strip():
+                continue
+            if a in b:
+                ans = a
+        if ans:
+            return ans
+
+    return 'NONE'
+
+f = open(argv[1], 'r')
+for one in f:
+    if one != '\n':
+        print max_repeated_substring(one)
+f.close()
