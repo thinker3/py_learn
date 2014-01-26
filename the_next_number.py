7b0cb6c3dba54b4c52cc88be7e3f6aa5a7f6a4e7
thinker3 <metathinkerk@gmail.com>
2014-01-26 12:22:32 +0800

    except print 0

---
+++ the_next_number.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+def get_the_next(one):
+    n = len(one)
+    for b in range(n-1,0,-1):
+        for a in range(b-1,-1,-1):
+            if one[a] < one[b]:
+                return one[:a] + one[b] + get_min(one[a:b] + one[b+1:])
+    return get_min(one, False)
+
+def get_min(left, flag=True):
+    left = [i for i in left]
+    left.sort()
+    if flag:
+        return ''.join(left)
+    else:
+        n = 0
+        while left[0] == '0':
+            left.pop(0)
+            n += 1
+        for i in range(n+1):
+            left.insert(1, '0')
+        return ''.join(left)
+
+f = open(argv[1], 'r')
+for one in f:
+    if one.strip():
+        try:
+            print get_the_next(one.strip())
+        except:
+            print 0 # ???
+f.close()
