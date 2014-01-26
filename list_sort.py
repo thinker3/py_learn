04e862bf6f55d1db97414ab1abd976f3558c6b0f
thinker3 <metathinkerk@gmail.com>
2014-01-26 21:32:49 +0800

    sort by key

--- list_sort.py
+++ list_sort.py
@@ -2,3 +2,16 @@ l=[1,5,3,2,7]
 print l
 print l.sort()
 print l
+
+l = [(2, 33.8, 40), (3, 43.15, 10), (4, 37.97, 16), (5, 46.81, 36), (6, 48.77, 79), (8, 19.36, 79), (9, 6.76, 64)]
+print l
+l.sort(key=lambda x: x[1])
+print l
+l.sort(key=lambda x: x[2])
+print l
+
+l = [(2, 33.8, 40), (3, 43.15, 10), (4, 37.97, 16), (5, 46.81, 36), (6, 48.77, 79), (8, 19.36, 79), (9, 6.76, 64)]
+l.sort(key=lambda x: x[2])
+print l
+l.sort(key=lambda x: x[1])
+print l
