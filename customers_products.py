87e2ed5dc617a855a8878656f5dd184140d6d3ae
thinker3 <metathinkerk@gmail.com>
2014-02-11 22:42:56 +0800

    even more cache

--- customers_products.py
+++ customers_products.py
@@ -8,7 +8,6 @@ def prepare(one):
     return customers, products 
 
 def get_letters(name):
-    global cache
     if cache.has_key(name+'_l'):
         return cache[name+'_l']
     num = 0
@@ -19,7 +18,6 @@ def get_letters(name):
     return num
 
 def get_vowels(name):
-    global cache
     if cache.has_key(name+'_v'):
         return cache[name+'_v']
     vowels = 'aeiouy'
@@ -33,18 +31,20 @@ def get_vowels(name):
     
 
 def get_ss(product, customer):
+    if cache.has_key('%s_%s' % (product, customer)):
+        return cache['%s_%s' % (product, customer)]
     if get_letters(product) % 2 == 0: # even
         ss = get_vowels(customer) * 1.5
     else: # odd
         ss = get_letters(customer) - get_vowels(customer)
     if has_common_factor(get_letters(customer), get_letters(product)):
         ss *= 1.5
+    cache['%s_%s' % (product, customer)] = ss
     return ss
 
 def has_common_factor(less, more):
     if less>more:
         less, more = more, less
-    global cache
     if cache.has_key('%s_%s' % (less, more)):
         return cache['%s_%s' % (less, more)]
     for i in range(2, less/2+1):
@@ -79,22 +79,15 @@ def get_max_ss(customers, products):
                 max_ss = ss if ss > max_ss else max_ss
     return max_ss
 
-cache = {'a': 1}
-def main():
-    f = open(argv[1], 'r')
-    for one in f:
-        if one.strip():
-            global cache
-            cache = {}
-            customers, products = prepare(one)
-            max_ss = get_max_ss(customers, products)
-            print '%.2f' % max_ss
-    f.close()
+f = open(argv[1], 'r')
+for one in f:
+    if one.strip():
+        cache = {}
+        customers, products = prepare(one)
+        max_ss = get_max_ss(customers, products)
+        print '%.2f' % max_ss
+f.close()
 
-def test():
-    print has_common_factor(1, 15)
-
-main()
 
 
 
