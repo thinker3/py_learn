3f5ab6ae3da40054640665a02baff815b297a6e7
thinker3 <metathinkerk@gmail.com>
2014-02-24 10:13:12 +0800

    todo

--- combinations.py
+++ combinations.py
@@ -1,5 +1,6 @@
+import itertools
 
-
+'''
 def my_combinations(n, r):
     if n<=0 or r<=0 or r>n:
         return []
@@ -18,7 +19,21 @@ def my_combinations(n, r):
                     temp.append(two)
         ans = temp
     return ans
+'''
+
+
+def my_combinations(n, r):
+    ans = range(r)
+    yield ans
+    while 1:
+        for i in xrange(r-1, -1, -1):
+            while ans[i] != n-r+i:
+                ans = ans[:i] + range(ans[i]+1, ans[i]+1+r-i)
+                yield ans
+
 
 for one in my_combinations(5, 2):
     print one
 
+for one in itertools.combinations(range(5), 3):
+    print one
