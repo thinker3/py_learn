cc6c9bf9fd5bc20718f218872a19b833c5d7ca76
thinker3 <metathinkerk@gmail.com>
2014-03-03 21:25:33 +0800

    combinational pairs

--- davyncy.py
+++ davyncy.py
@@ -1,15 +1,5 @@
-src = \
-'''
-O draconia;conian devil! Oh la;h lame sa;saint!
-
-m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al
-'''
-
-'''
-O draconian devil! Oh lame saint!
-
-Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-'''
+import itertools
+from sys import argv
 
 def get_overlap_number_sub(a, b):
     len_a, len_b = map(len, (a, b))
@@ -21,22 +11,16 @@ def get_overlap_number_sub(a, b):
             return 1
         for i in xrange(1, min(le_a, len_b)):
             if a[i+j] != b[i]:
-                return 0
+                return get_overlap_number_sub(a[j+1:], b)
         else:
             return i + 1
     except:
         return 0
 
+
 def get_overlap_number(a, b):
-    #m = max(get_overlap_number_sub(a, b), get_overlap_number_sub(b, a))
-    i = get_overlap_number_sub(a, b)
-    j = get_overlap_number_sub(b, a)
-    if i>j:
-        return i
-    elif i<j:
-        return j
-    else:
-        return 0
+    m = max(get_overlap_number_sub(a, b), get_overlap_number_sub(b, a))
+    return m
 
 def merge(a, b):
     i = get_overlap_number_sub(a, b)
@@ -46,40 +30,29 @@ def merge(a, b):
     else:
         return b + a[j:]
 
+def concat(one):
+    fragments = one.split(';')
+    while len(fragments) > 1:
+        n = len(fragments)
+        the_max = 0
+        index = None
+        for i, j in itertools.combinations(range(n), 2):
+            m = get_overlap_number(fragments[i], fragments[j])
+            if m > the_max:
+                the_max = m
+                index = i, j
+        if index:
+            i, j = index
+            merged = merge(fragments[i], fragments[j])
+            fragments.pop(j)
+            fragments.pop(i)
+            fragments.append(merged)
+    print fragments[0]
 
-from time import sleep
-for one in src.split('\n'):
+f = open(argv[1], 'r')
+for one in f:
+    one = one.strip()
     if one:
-        fragments = one.split(';')
-        temp = []
-        while len(fragments) + len(temp)>1:
-            n = len(fragments)
-            if n <= 1:
-                fragments = fragments + temp
-                temp = []
-                continue
-            sleep(0.1)
-            the_max = 0
-            index = 0
-            for i in xrange(1, n):
-                m = get_overlap_number(fragments[0], fragments[i])
-                if m > the_max:
-                    the_max = m
-                    index = i
-            if index:
-                merged = merge(fragments[0], fragments[index])
-                fragments.pop(index)
-                fragments.pop(0)
-                temp.append(merged)
-            else:
-                s0 = fragments.pop(0)
-                temp.append(s0)
-            print fragments, temp
-
-
-
-
-
-
-
+        concat(one)
+f.close()
 
