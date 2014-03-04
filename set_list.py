2bf34db4adcdb159903390c44ed556b2105b87eb
thinker3 <metathinkerk@gmail.com>
2014-03-04 15:48:56 +0800

    set | & -

--- set_list.py
+++ set_list.py
@@ -16,3 +16,15 @@ print s
 t=tuple()
 print t
 # t.append(1) #no attr append, since tuples are immutable
+
+# set union intersection difference | & -
+a = set([1, 2, 3, 4])
+b = set([1, 4, 7])
+print a, b
+print a|b
+print a&b
+print a-b
+
+
+
+
