f7dbf1bf517c4f627f6669a682b32c063548a4cd
thinker3 <metathinkerk@gmail.com>
2014-03-10 13:26:03 +0800

    done?

--- decryption.py
+++ decryption.py
@@ -1,16 +1,25 @@
+import string
 message = "012222 1114142503 0313012513 03141418192102 0113 2419182119021713 06131715070119"
 keyed_alphabet = "BHISOECRTMGWYVALUZDNFJKPQX"
 message = list(message)
+caps = string.letters[26:]
+
+
+indexes = []
+for one in caps:
+    i = keyed_alphabet.index(one)
+    indexes.append(i)
 
 
 def get_message():
     while message:
         one = message.pop(0)
         if one == ' ':
-            yield ' '
+            yield 26
         else:
             one += message.pop(0)
             one = int(one)
-            yield keyed_alphabet[one]
+            yield indexes[one]
 
-print ''.join(get_message())
+caps += ' '
+print ''.join(map(lambda x: caps[x], get_message()))
