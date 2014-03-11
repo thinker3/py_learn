db295688a6f2067a97d1e76c333125f7039f1ab7
thinker3 <metathinkerk@gmail.com>
2014-03-11 14:18:44 +0800

    add 0 if not enough

--- rjust.py
+++ rjust.py
@@ -1,2 +1,6 @@
-print "3".rjust(2,'0')
-print "3".rjust(3,'0')
+print "3".rjust(2, '0')
+print "3".rjust(3, '0')
+n = 100
+ds = len(str(n))
+numbers = map(lambda i: str(i).rjust(ds, '0'), range(1, n))
+print ' '.join(numbers)
