ce1d5e1d0d911269bf8bae04e3797b99b7c7a8ab
thinker3 <metathinkerk@gmail.com>
2014-03-14 16:16:19 +0800

    levenshtein distance is hard to understand

--- dna.py
+++ dna.py
@@ -1,18 +1,32 @@
 from sys import argv
 import operator
 
-'''
-CCC CCA CCG CGC GCC TCC
-GCAAG GCAAG GCCAG GCGCG GCGCA GCTAA
-No match
-'''
 
+def levenshtein(s1, s2):
+    '''
+    copied from wikibooks
+    '''
+    if len(s1) < len(s2):
+        s1, s2 = s2, s1
+        #return levenshtein(s2, s1)
 
-def get_distance(segment, temp):
-    #genes = 'ACGT'
-    if segment == temp:
-        return 0
-    return 0
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            # j+1 instead of j
+            # since previous_row and current_row are one character longer
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
 
 
 def get_segments(one):
@@ -22,7 +36,7 @@ def get_segments(one):
     less, more = len(segment), len(dna)
     for i in xrange(more - less + 1):
         temp = dna[i: i + less]
-        d = get_distance(segment, temp)
+        d = levenshtein(segment, temp)
         if d <= m:
             ans.append((d, temp))
     return ans
