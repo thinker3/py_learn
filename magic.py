de4993ec3e54414c2ef9d1baac52438c39d3ee84
thinker3 <metathinkerk@gmail.com>
2014-03-23 15:08:22 +0800

    more comments

--- magic.py
+++ magic.py
@@ -1,56 +1,83 @@
 import os
 
 # run in this floder and another folder respectively
-print __file__, type(__file__)
+print __file__, type(__file__)  # magic.py <type 'str'>
 filepath = os.path.abspath(__file__)
 print filepath
 print os.path.dirname(filepath)
 print os.path.dirname(__file__)
 
 print '-' * 100
-print __name__, type(__name__)
+print __name__, type(__name__)  # __main__ <type 'str'>
 
 
 print '-' * 100
+
+
 class Foo(object):
-    def __new__(self):
+
+    def __new__(cls):
         print 1
 
-    def __init__(self):
-        print 2
+    def __init__(cls):
+        print 2  # not be printed
 
 f = Foo()
 
 print '-' * 100
+
+
 class Foo(object):
-    def __new__(self):
+
+    def __new__(cls):
         print 1
-        return object.__new__(self)
+        return object.__new__(cls)
 
     def __init__(self):
         print 2
 
+    def __call__(self):
+        print 3
+
 f = Foo()
+f()  # __call__ called here
 
 print '-' * 100
+
+
 class Foo(object):
-    def __new__(self, name):
-        print self
-        self.name = name
-        return object.__new__(self)
+
+    def __new__(cls, name):
+        print cls  # <class '__main__.Foo'>
+        cls.name = name
+        what = object.__new__(cls)
+        print what  # <__main__.Foo object at 0x7f16ff4831d0>
+        return what
+
+    '''
+    # TypeError: __new__() takes exactly 1 argument (2 given)
+    def __new__(cls):
+        return object.__new__(cls)
+    '''
 
     def __init__(self, name):
-        print self
-        print self.name
+        print self  # <__main__.Foo object at 0x7f9f29cc3250>
+        print self.name  # Jim
         self.name = name * 2
-        print self.name
+        print self.name  # JimJim
+        print self.__class__.name  # Jim
 
-Foo('Jim')
+f = Foo('Jim')
+print f.__dict__
+print Foo.__dict__
 
 print '-' * 100
+
+
 class Foo(object):
+
     def __new__(cls, name):
-        print cls 
+        print cls
         cls.name = name
         return object.__new__(cls, name, 'more', 'even more')
 
@@ -63,11 +90,22 @@ class Foo(object):
 Foo('Jim')
 
 print '-' * 100
+
+
 class Meta(type):
+
     def __call__(cls, *args, **kwargs):
-        obj = cls.__new__(cls, *args, **kwargs)
+        print cls  # <class '__main__.Foo'>
+        print args  # ('a',)
+        print kwargs  # {}
+        obj = cls.__new__(cls, *args, **kwargs)  # 1
+        print obj  # <__main__.Foo object at 0x7f7e4d90b350>
+        print obj.__dict__  # {}
+        print 'before'
         #obj.__init__(*args, **kwargs)
+        print 'after'
         return obj
+
 
 class Foo(object):
     __metaclass__ = Meta
@@ -77,24 +115,29 @@ class Foo(object):
         return object.__new__(cls, *args, **kwargs)
 
     def __init__(self, *args, **kwargs):
-        print 2
+        print 2  # be printed only if explicitly called
 
-    def show(self): print 'hello'
+    def show(self):
+        print 'hello'
 
 f = Foo('a')
 f.show()
 
 print '-' * 100
 Foo.__new__(Foo)
-#Foo.__new__()
-#Foo.__init__(Foo)
+# Foo.__new__()
+# Foo.__init__(Foo)
 
 print '-' * 100
+
+
 class Meta(type):
+
     def __call__(cls):
         obj = cls.__new__(cls)
         obj.__init__()
         return obj
+
 
 class Foo(object):
     __metaclass__ = Meta
@@ -107,22 +150,22 @@ class Foo(object):
         print 2
 
     @classmethod
-    def show(cls): print cls
+    def show(cls):
+        print cls
 
 f = Foo()
-f.show()
+f.show()  # <class '__main__.Foo'>
+print f.__new__  # <function __new__ at ...>
+print Foo.__new__  # <function __new__ at ...>
 f.__new__(Foo)
-f.__init__()
 Foo.__new__(Foo)
-print f.__new__
-print Foo.__new__
+print Foo.__init__  # <unbound method Foo.__init__>
+# <bound method Foo.__init__ of <__main__.Foo object at ...>>
 print f.__init__
-print Foo.__init__
-print f.show
-print Foo.show
-#Foo.__new__()
-#Foo.__init__(Foo)
-
-
-
-
+f.__init__()  # 2
+print f.show  # <bound method Meta.show of <class '__main__.Foo'>>
+print Foo.show  # <bound method Meta.show of <class '__main__.Foo'>>
+"""
+f.__new__()  # TypeError: __new__() takes exactly 1 argument (0 given)
+Foo.__new__()  # TypeError: __new__() takes exactly 1 argument (0 given)
+"""
