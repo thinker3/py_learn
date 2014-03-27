33961f0466b9b1a86764135dc8256621b0b79888
thinker3 <metathinkerk@gmail.com>
2014-03-27 15:59:41 +0800

    and or is not ==

---
+++ and_is_not_or.py
@@ -0,0 +1,23 @@
+# find the first bad one, or the last one
+print 0 and '' and ()
+print 1 and () and []
+print 2 and 1 and 'a'
+
+print '*' * 30
+# find the first good one, or the last one
+print 1 or 2 or []
+print 0 or 2 or []
+print 0 or '' or []
+
+print '*' * 30
+print 1 is 1
+print 1 is 2
+print 1 is not 2
+print 1 is not 1
+print False is (not 1)
+print 1 is (not 1)
+#print 1 (is not) 1  # SyntaxError: invalid syntax
+
+print '*' * 30
+print 1 == True
+print 1 == False
