8eb60a1d357afafc63acb40fb98a72c91091ee31
thinker3 <metathinkerk@gmail.com>
2014-04-03 14:05:12 +0800

    maxsplit seq

--- split_re_unicode.py
+++ split_re_unicode.py
@@ -1,4 +1,4 @@
-#coding=utf8
+# coding=utf8
 import re
 s_list = [
     'abc,edf',
@@ -8,9 +8,16 @@ s_list = [
     'abc  , edf',
     'abc ,  edf',
     'abc :  edf',
-    u'abc，edf', # a leading 'u' is needed
-    ]
+    u'abc，edf',  # a leading 'u' is needed
+]
 p = re.compile(ur'\s*[,:，]\s*')
 for one in s_list:
     r = p.split(one)
     print r
+
+s = 'a;b;c;d'
+print s.split(';', 1)  # ['a', 'b;c;d']
+print s.split(';', 2)  # ['a', 'b', 'c;d']
+
+s = 'a__b__c__d'
+print s.split('__')
