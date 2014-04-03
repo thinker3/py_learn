9b1245715887c864ec016597fbae218c27c07445
thinker3 <metathinkerk@gmail.com>
2014-04-03 17:24:24 +0800

    iteritems

--- dict_key_no_quotes.py
+++ dict_key_no_quotes.py
@@ -1,9 +1,9 @@
 a = (1, 2, 3)
 b = ('jack', 'tom')
 d = {
-        a: 'abc',
-        b: 'edf',
-        }
+    a: 'abc',
+    b: 'edf',
+}
 
 for k, v in d.items():
     print k, v
@@ -17,4 +17,7 @@ print d[True]
 print d[False]
 
 li = [(k, v) for k, v in d.items()]
-print li
+print li  # [(False, '123'), (True, 'abc')]
+
+print d.items()  # [(False, '123'), (True, 'abc')]
+print d.iteritems()  # <dictionary-itemiterator object at 0x7f9456456f18>
