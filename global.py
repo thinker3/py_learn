f2db2dfcfd0bc23bf0e71028dd107965ec572ebe
thinker3 <metathinkerk@gmail.com>
2014-04-08 22:39:32 +0800

    define a local variable the same name as a global variable

--- global.py
+++ global.py
@@ -1,64 +1,53 @@
+num = 100
+
+
+def func():
+    print num  # global 'num' used
+func()
+
+
+def foo():
+    num = 1  # local 'num' defined
+    print num
+foo()
+
+
+'''
+def bar():
+    # UnboundLocalError: local variable 'num' referenced before assignment
+    num = num + 1
+    num += 1
+    print num
+bar()
+'''
+
 total = 0
+
 
 def add(n):
     global total
     for i in xrange(n):
-        total += (i+1)
+        total += (i + 1)  # global 'total' changed
     print total
-
 add(100)
-print
-print total
-print
-
-total = 0
-for i in xrange(10):
-    total += i+1
-print total
 print '-' * 20
+print num
 
 
-def test():
-    print total # 'total' is made local before calling test()???
-    total = 0
-
-import traceback
-try:
-    test()
-except:
-    traceback.print_exc()
-print '-' * 20
-
-
-def test():
-    total += 0
-
-import traceback
-try:
-    test()
-except:
-    traceback.print_exc()
-print '-' * 20
+'''
+def test(num):
+    global num  # SyntaxError: name 'num' is local and global
+'''
 
 
 def outer(num):
     def inner():
         global num
-        num = 2
+        num = 2  # global 'num' changed
         print num, 'inner'
-    print num
+    # swap the following two lines
+    print num, 'outer'  # local 'num' used
     inner()
 
 outer(1)
 print num + 1
-print '-' * 20
-
-#def test(num):
-#    global num # SyntaxError: name 'num' is local and global
-
-
-
-
-
-
-
