b29d607a9ec75b60f0ba18d16bd680b78c6d1e50
thinker3 <metathinkerk@gmail.com>
2014-04-19 23:41:16 +0800

    resize or thumbnail

---
+++ challenges/shrink.py
@@ -0,0 +1,41 @@
+import Image
+from PngImagePlugin import PngImageFile
+from JpegImagePlugin import JpegImageFile
+clz = PngImageFile
+clz = JpegImageFile
+
+
+def get_ext(im):
+    return im.format.lower()
+
+
+def resize(im):
+    assert isinstance(im, clz)
+    output = 'thumbnail.%s' % get_ext(im)
+    x = 400
+    y = x
+    # im = im.resize((x, y), Image.ANTIALIAS)
+    im = im.resize((x, y))  # smaller
+    im.save(output)
+
+
+def thumbnail(im):
+    assert isinstance(im, clz)
+    x, y = im.size
+    x = x / 5  # how about y? no effect?
+    # im.thumbnail((x, y))
+    im.thumbnail((x, y), Image.ANTIALIAS)  # smaller
+    # im.save('thumbnail.png', 'JPEG')  # wrong
+    # im.save('thumbnail.png', 'PNG')  # ok
+    output = 'thumbnail.%s' % get_ext(im)
+    im.save(output, im.format)
+
+
+def main(filename='oxygen.png'):
+    im = Image.open(filename)
+    assert isinstance(im, clz)
+    resize(im)
+    # thumbnail(im)
+
+filename = 'rail.jpg'
+main(filename)
