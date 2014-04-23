f854a63b18929292167e64b653852fb012d1f8f0
thinker3 <metathinkerk@gmail.com>
2014-04-23 18:01:49 +0800

    old, new style classes, assignment overloading, replace method at runtime

--- overload.py
+++ overload.py
@@ -10,3 +10,64 @@ print a == ''
 print a == True
 print a == None
 print a == 'Hello!'
+print '*' * 50
+
+# old, new style classes, assignment overloading, replace method at runtime
+
+class Base:
+    def __setattr__(self, attr, value):
+        self.__dict__[attr] = 'base'
+
+
+class Extend(Base):
+    pass
+
+_setattr_ = Base.__setattr__
+#_setattr_ = Extend.__setattr__
+
+
+def my_setattr_(self, attr, value):
+    print 'called'
+    _setattr_(self, attr, value)
+    self.__dict__[attr*2] = value
+
+e = Extend()
+b = Base()
+e.foo = 'foo'
+print e.__dict__
+Base.__setattr__ = my_setattr_
+#Extend.__setattr__ = my_setattr_
+e.foo = 'foo'
+print e.__dict__
+b.bar = 'bar'
+print b.__dict__
+print '*' * 50
+
+
+class Base(object):
+    def __setattr__(self, attr, value):
+        object.__setattr__(self, attr, 'base')
+
+
+class Extend(Base):
+    pass
+
+_setattr_ = Base.__setattr__
+#_setattr_ = Extend.__setattr__
+
+
+def my_setattr_(self, attr, value):
+    print 'called'
+    _setattr_(self, attr, value)
+    object.__setattr__(self, attr*2, value)
+
+e = Extend()
+b = Base()
+e.foo = 'foo'
+print e.__dict__
+Base.__setattr__ = my_setattr_
+#Extend.__setattr__ = my_setattr_
+e.foo = 'foo'
+print e.__dict__
+b.bar = 'bar'
+print b.__dict__
