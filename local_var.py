ba4d8282af708a4e654066f0c9f93c974a5f4c8f
thinker3 <metathinkerk@gmail.com>
2014-04-23 21:36:14 +0800

    locals()

--- local_var.py
+++ local_var.py
@@ -10,4 +10,32 @@ t = 0
 for i in xrange(10):
     t = t + i
 print t
+print '*' * 50
 
+
+class A(object):
+    def __init__(self, num):
+        self.num = num
+
+
+class B(object):
+    def func(self):
+        a = A(0)
+        b = 1
+        print locals()
+        for k, v in locals().items():
+            if isinstance(v, A):
+                print v.num
+
+        def inner():
+            a = A(10)
+            b = 20
+            print locals()
+            for k, v in locals().items():
+                if isinstance(v, A):
+                    print v.num
+                    print locals()
+        inner()
+
+b = B()
+b.func()
