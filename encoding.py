c7c9875ce175558a034604a6b178c786a13f32e8
thinker3 <metathinkerk@gmail.com>
2014-06-09 17:17:16 +0800

    sys locale encoding

---
+++ encoding.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys 
+import locale
+
+
+def p(f):
+    print '%s.%s(): %s' % (f.__module__, f.__name__, f())
+
+p(sys.getdefaultencoding)
+p(sys.getfilesystemencoding)
+p(locale.getdefaultlocale)
+p(locale.getpreferredencoding)
