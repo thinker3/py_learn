b4aaea9f0aee8daa0f5c1ea326b6d935aff2df3b
thinker3 <metathinkerk@gmail.com>
2014-07-01 12:59:19 +0800

    pprint

--- print.py
+++ print.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+# pretty print
 
-for i in range(10):
-	print i
+import pprint 
 
+out = {'a':2, 'b':{'x':3, 'y':{'t1': 4, 't2':5}}}
+pprint.pprint(out)
+#pprint.pprint(out, width=0)  # AssertionError: width must be != 0
+pprint.pprint(out, width=1)
