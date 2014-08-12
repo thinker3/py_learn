2985d428ed6a4d7897f016ba4dec419f6f838597
thinker3 <metathinkerk@gmail.com>
2014-08-12 18:17:33 +0800

    gevent first

---
+++ thread/gevent_async.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# SystemError: (libev) select: Unknown error
+import socket
+
+import gevent
+import random
+
+def task(pid):
+    gevent.sleep(random.randint(0, 9) * 0.1)
+    print('Task %s done' % pid)
+
+def synchronous():
+    for i in range(1,10):
+        task(i)
+
+def asynchronous():
+    threads = [gevent.spawn(task, i) for i in xrange(10)]
+    gevent.joinall(threads)
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
---
+++ thread/gevent_monkey.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import gevent.monkey
+gevent.monkey.patch_socket()
+
+import socket
+import gevent
+import urllib2
+import simplejson as json
+
+def fetch(pid):
+    protocol = 'http'
+    proxy = urllib2.ProxyHandler({protocol: '127.0.0.1:8087'})
+    opener = urllib2.build_opener(proxy)
+    urllib2.install_opener(opener)
+    url = '%s://date.jsontest.com' % protocol
+    response = urllib2.urlopen(url)
+    result = response.read()
+    json_result = json.loads(result)
+    time = json_result['time']
+
+    print('Process %s: %s' % (pid, time))
+    return json_result['time']
+
+def synchronous():
+    for i in range(1,10):
+        fetch(i)
+
+def asynchronous():
+    threads = []
+    for i in range(1,10):
+        threads.append(gevent.spawn(fetch, i))
+    gevent.joinall(threads)
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
