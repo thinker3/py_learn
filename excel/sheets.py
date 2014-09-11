3b2db9e6660151da314f41d9dc7a456c071167e7
thinker3 <metathinkerk@gmail.com>
2014-09-11 18:43:00 +0800

    add sheets

---
+++ excel/sheets.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import win32com.client as win32
+import pythoncom
+
+
+def main():
+    #pythoncom.CoInitialize()
+    excel = win32.DispatchEx('Excel.Application')
+    print excel.Version
+    excel.Visible = 1
+    excel.DisplayAlerts = False
+    workBook = excel.Workbooks.Add()
+    #sheet = workBook.ActiveSheet
+    sheet = workBook.Sheets(1)
+    sheet.Name = 'a'
+    sheet.Cells(1, 1).Value = 'a'
+    sheet = workBook.Sheets.Add()
+    sheet.Name = 'b'
+    sheet.Cells(1, 1).Value = 'b'
+    sheet = workBook.Sheets.Add(After=workBook.Sheets(workBook.Sheets.Count))
+    sheet.Name = 'c'
+    workBook.Sheets(3).Select()
+
+
+if __name__ == '__main__':
+    main()
