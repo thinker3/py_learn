1dddeb7815eaf3b46c1ec02c58d5b697b265cd26
thinker3 <metathinkerk@gmail.com>
2014-09-23 06:22:17 +0800

    exceptions

--- windows/quick_edit.py
+++ windows/quick_edit.py
@@ -11,16 +11,18 @@ hin = win32.GetStdHandle(-10)
 mode = ctypes.c_int(0)
 win32.GetConsoleMode(hin, ctypes.byref(mode))
 old_mode = mode.value
-# disable quick edit mode
+# disable Windows console(cmd.exe) quick edit mode
 new_mode = old_mode & (~0x0040)
 win32.SetConsoleMode(hin, new_mode)
 
 running = True
 while running:
     try:
-        r = random.randint(1, 100)
+        r = random.randint(1, 7)
         print 'hello %s' % r
+        if r == 4:
+            1/0
         time.sleep(1)
-    except KeyboardInterrupt:
+    except (Exception, KeyboardInterrupt):
         win32.SetConsoleMode(hin, old_mode)
         running = False
