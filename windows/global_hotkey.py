9980f3f67163799a1c424b564a1ce95160d6a826
thinker3 <metathinkerk@gmail.com>
2014-09-27 18:56:30 +0800

    working edition

--- windows/global_hotkey.py
+++ windows/global_hotkey.py
@@ -1,30 +1,64 @@
-import os
-import sys
+#!/usr/bin/env python
+# encoding: utf-8
+
 import ctypes
-from ctypes import wintypes
+import ctypes.wintypes as wt
 import win32con
 
-byref = ctypes.byref
 user32 = ctypes.windll.user32
 
-id = 1
-#if not user32.RegisterHotKey(None, id, win32con.MOD_WIN, win32con.VK_SHIFT):
-r = user32.RegisterHotKey(None, id, win32con.MOD_CONTROL, win32con.VK_F1)
-print r
-if not r:
-    print "Unable to register id", id
 
-id += 1
-r = user32.RegisterHotKey(None, id, win32con.MOD_CONTROL | win32con.MOD_ALT, 0x07)
-print r
-if not r:
-    print "Unable to register id", id
+class GlobalHotKey(object):
+    running = True
+    key_mapping = []
 
-msg = wintypes.MSG()
-while user32.GetMessageA(byref(msg), None, 0, 0) != 0:
-    print msg, msg.message
-    if msg.message == win32con.WM_HOTKEY:
+    MOD_ALT = win32con.MOD_ALT
+    MOD_CTRL = win32con.MOD_CONTROL
+    MOD_CONTROL = win32con.MOD_CONTROL
+    MOD_SHIFT = win32con.MOD_SHIFT
+    MOD_WIN = win32con.MOD_WIN
+
+    @classmethod
+    def register(cls, vk, modifier=0, func=None):
+        key_code = ord(vk.upper())
+        if func is None:
+            def register_decorator(f):
+                cls.register(vk, modifier, f)
+                return f
+            return register_decorator
+        else:
+            cls.key_mapping.append((key_code, modifier, func))
+
+    @classmethod
+    def listen(cls):
+        for index_id, (key_code, modifiers, func) in enumerate(cls.key_mapping):
+            user32.RegisterHotKey(None, index_id, modifiers, key_code)
+        try:
+            msg = wt.MSG()
+            while cls.running and user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
+                if msg.message == win32con.WM_HOTKEY:
+                    (key_code, modifiers, func) = cls.key_mapping[msg.wParam]
+                    if not func:
+                        break
+                    func()
+                user32.TranslateMessage(ctypes.byref(msg))
+                user32.DispatchMessageA(ctypes.byref(msg))
+        finally:
+            for index_id, (key_code, modifiers, func) in enumerate(cls.key_mapping):
+                user32.UnregisterHotKey(None, index_id)
+
+def main():
+    # press ctrl-c to quit
+    GlobalHotKey.register('c', GlobalHotKey.MOD_CTRL, False)
+
+
+    @GlobalHotKey.register('c', GlobalHotKey.MOD_ALT)
+    def test():
         print 'hello'
 
-user32.TranslateMessage(byref(msg))
-user32.DispatchMessageA(byref(msg))
+
+    GlobalHotKey.listen()
+
+
+if __name__ == '__main__':
+    main()
