88683bbb9b2626a99e1cfad431262660ca335763
thinker3 <metathinkerk@gmail.com>
2014-11-23 16:44:29 +0800

    sort(reverse=True)

---
+++ codeeval/roman.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from sys import argv
+
+base = [1, 5, 10, 50, 100, 500, 1000]
+letters = "I, V, X, L, C, D, M".split(', ')
+
+d = {}
+for i in range(len(base) - 1):
+    d[base[i]] = letters[i]
+    if i % 2 == 0:
+        d[base[i] * 4] = letters[i] + letters[i + 1]
+    else:
+        d[base[i] * 9 / 5] = letters[i - 1] + letters[i + 1]
+d[base[i + 1]] = letters[i + 1]
+base_r = d.keys()
+base_r.sort(reverse=True)
+
+'''
+print base_r
+for i, j in d.iteritems():
+    print i, j
+'''
+
+
+def to_roman(n):
+    first = []
+    m = n
+    for i in base_r:
+        while m - i >= 0:
+            m -= i
+            first.append(d[i])
+    return ''.join(first)
+
+f = open(argv[1], 'r')
+for one in f:
+    one = one.strip()
+    if one:
+        one = int(one)
+        print to_roman(one)
+f.close()
