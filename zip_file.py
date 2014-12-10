987aaf03ef94093c13f573f1df80aeca8bd6fe02
thinker3 <metathinkerk@gmail.com>
2014-12-10 14:30:04 +0800

    multiple_levels_zipped

--- zip_file.py
+++ zip_file.py
@@ -4,12 +4,15 @@
 import os
 import zipfile
 
+dirname = os.path.dirname(os.path.abspath(__file__))
+target_dirname = os.path.expanduser('~')
+target_filename = 'test_zipfile.zip'
+
 
 def one_file_zipped():
-    dirname = os.path.dirname(os.path.abspath(__file__))
     basename = 'class_static_method.py'
     fullname = os.path.join(dirname, basename)
-    output = os.path.join(dirname, 'test.zip')
+    output = os.path.join(target_dirname, target_filename)
 
     dst = zipfile.ZipFile(output, 'w')
     dst.write(fullname, basename)
@@ -17,9 +20,8 @@ def one_file_zipped():
 
 
 def more_files_zipped():
-    dirname = os.path.dirname(os.path.abspath(__file__))
     files = ['class_static_method.py', 'matrix.py', 'get_obj_by_method.py']
-    output = os.path.join(dirname, 'test.zip')
+    output = os.path.join(target_dirname, target_filename)
 
     dst = zipfile.ZipFile(output, 'w')
     for basename in files:
@@ -31,6 +33,27 @@ def more_files_zipped():
     dst.close()
 
 
+def multiple_levels_zipped():
+    files = [
+        ['class_static_method.py', 'matrix.py',],
+        ['get_obj_by_method.py'],
+        ]
+    first = 'abc'
+    second = [
+            'hello',
+            'world',
+            ]
+    output = os.path.join(target_dirname, target_filename)
+
+    dst = zipfile.ZipFile(output, 'w')
+    for i, second_dir in enumerate(second):
+        for basename in files[i]:
+            fullname = os.path.join(dirname, basename)
+            arcname = os.path.join(first, second_dir, basename)
+            dst.write(fullname, arcname, zipfile.ZIP_DEFLATED)
+    dst.close()
+
 if __name__ == '__main__':
-    one_file_zipped()
-    more_files_zipped()
+    #one_file_zipped()
+    #more_files_zipped()
+    multiple_levels_zipped()
