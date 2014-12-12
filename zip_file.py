a429608d32fdc6c20720df7659567ebe132ac3bf
thinker3 <metathinkerk@gmail.com>
2014-12-12 17:14:01 +0800

    if zip has content

--- zip_file.py
+++ zip_file.py
@@ -7,12 +7,14 @@ import zipfile
 dirname = os.path.dirname(os.path.abspath(__file__))
 target_dirname = os.path.expanduser('~')
 target_filename = 'test_zipfile.zip'
+output = os.path.join(target_dirname, target_filename)
+if os.path.exists(output):
+    os.remove(output)
 
 
 def one_file_zipped():
     basename = 'class_static_method.py'
     fullname = os.path.join(dirname, basename)
-    output = os.path.join(target_dirname, target_filename)
 
     dst = zipfile.ZipFile(output, 'w')
     dst.write(fullname, basename)
@@ -21,7 +23,6 @@ def one_file_zipped():
 
 def more_files_zipped():
     files = ['class_static_method.py', 'matrix.py', 'get_obj_by_method.py']
-    output = os.path.join(target_dirname, target_filename)
 
     dst = zipfile.ZipFile(output, 'w')
     for basename in files:
@@ -35,15 +36,14 @@ def more_files_zipped():
 
 def multiple_levels_zipped():
     files = [
-        ['class_static_method.py', 'matrix.py',],
+        ['class_static_method.py', 'matrix.py'],
         ['get_obj_by_method.py'],
         ]
     first = 'abc'
     second = [
-            'hello',
-            'world',
-            ]
-    output = os.path.join(target_dirname, target_filename)
+        'hello',
+        'world',
+        ]
 
     dst = zipfile.ZipFile(output, 'w')
     for i, second_dir in enumerate(second):
@@ -51,9 +51,22 @@ def multiple_levels_zipped():
             fullname = os.path.join(dirname, basename)
             arcname = os.path.join(first, second_dir, basename)
             dst.write(fullname, arcname, zipfile.ZIP_DEFLATED)
+    print dst.filelist
     dst.close()
+
+
+def zero_file_zipped():
+    dst = zipfile.ZipFile(output, 'w')
+    print dst.filelist
+    has_content = bool(dst.filelist)
+    print has_content
+    dst.close()
+    if not has_content:
+        os.remove(output)
+
 
 if __name__ == '__main__':
     #one_file_zipped()
     #more_files_zipped()
-    multiple_levels_zipped()
+    #multiple_levels_zipped()
+    zero_file_zipped()
