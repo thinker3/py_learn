3723c9402b338e81c30f85f9a0ecbac05105f696
thinker3 <metathinkerk@gmail.com>
2014-12-21 17:09:10 +0800

    requests_learn.py reviewed

--- requests_learn.py
+++ requests_learn.py
@@ -1,29 +1,71 @@
 #coding=utf8
 
-import requests
+import os
 import json
+import requests
 
-url = 'http://www.baidu.com'
-r = requests.get(url)
-print dir(r)
-#print r.text # html
-print r.url
-print r.links
-print r.raw # <requests.packages.urllib3.response.HTTPResponse object at 0x1cd0990>
+home = os.path.expanduser('~')
 
-print '*' * 80
-print
-url = 'http://httpbin.org/post'
-payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.post(url, data=payload)
-print r.text
-print r.json()
 
-print '*' * 80
-print
-url = 'http://httpbin.org/post'
-payload = {'key1': 'value1', 'key2': 'value2'}
-headers = {'content-type': 'application/json'}
-r = requests.post(url, data=json.dumps(payload), headers=headers)
-print r.text
-print r.json()
+def separate(num=80):
+    print '*' * num
+
+
+def test_get():
+    separate()
+    url = 'http://www.baidu.com'
+    r = requests.get(url)
+    assert isinstance(r, requests.Response)
+    #print dir(r)
+    print r.url
+    print r.headers
+    print r.cookies
+    print type(r.status_code)  # <type 'int'>
+    print r.status_code  # 200
+    print type(r.text)  # <type 'unicode'>
+    print len(r.text)  # 85994
+    print type(r.content)  # <type 'str'>
+    print len(r.content)  # 86207
+    with open(os.path.join(home, 'requests_content'), 'w') as f:
+        f.write(r.content)
+    #print type(r.json())  # ValueError: No JSON object could be decoded
+    #print r.json()
+    print type(r.links)  # <type 'dict'>
+    print r.links  # {}
+    print type(r.raw)  # <class 'urllib3.response.HTTPResponse'>
+    print r.raw  # <requests.packages.urllib3.response.HTTPResponse object at 0x1cd0990>
+
+
+def test_post():
+    separate()
+    url = 'http://httpbin.org/post'
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.post(url, data=payload)
+    assert isinstance(r, requests.Response)
+    print r.text
+    separate(30)
+    print r.content
+    separate(30)
+    print type(r.json())  # <type 'dict'>
+    print r.json()
+
+
+def test_post_json():
+    separate()
+    url = 'http://httpbin.org/post'
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    assert isinstance(r, requests.Response)
+    print r.text
+    separate(30)
+    print r.content
+    separate(30)
+    print type(r.json())  # <type 'dict'>
+    print r.json()
+
+#test_get()
+#test_post()
+test_post_json()
+
+separate()
