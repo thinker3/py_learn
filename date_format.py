5d35f289912edc4c22db08e6049ae292e864a691
thinker3 <metathinkerk@gmail.com>
2014-12-22 10:33:35 +0800

    date_format.py reviewed

--- date_format.py
+++ date_format.py
@@ -1,29 +1,57 @@
-from datetime import datetime
-from time import sleep
+#!/usr/bin/env python
+#coding: utf8
 
-s = '20130824 09:32:12'
-time = datetime.strptime(s, '%Y%m%d %H:%M:%S')
-print time
-print type(time)
+# http://strftime.org/
 
-s = datetime.strftime(time, '%Y/%m/%d %H:%M:%S')
-print s
-print type(s)
+import time
+import datetime as dt
 
-before = datetime.now()
-sleep(1)
-now = datetime.now()
-delta = now - before
-print '%s seconds' % delta.total_seconds()
-print '%f seconds' % delta.total_seconds()
 
-print
-some_day = datetime.strptime('07/13/2007', '%m/%d/%Y')
-some_day = some_day.strftime('%m %B %Y')
-print some_day
+def parse_format():
+    now = dt.datetime.now()
+    s = dt.datetime.strftime(now, '%Y_%m_%d__%H_%M_%S__%f')
+    print s
+    print
 
-some_day = datetime.strptime('May 5 2007', '%B %d %Y').date()
-first_day = datetime.strptime('01/01/2007', '%m/%d/%Y').date()
-day_delta = (some_day - first_day).days + 1
-print some_day, first_day
-print day_delta
+    s = '20130824 09:32:12'
+    time = dt.datetime.strptime(s, '%Y%m%d %H:%M:%S')
+    print time
+    print type(time)  # <type 'datetime.datetime'>
+    print
+
+    s = dt.datetime.strftime(time, '%Y/%m/%d %H:%M:%S')
+    print s
+    print type(s)  # <type 'str'>
+    print
+
+    some_day = dt.datetime.strptime('07/13/2007', '%m/%d/%Y')
+    some_day = some_day.strftime('%m %B %Y')
+    print some_day
+
+
+def total_seconds():
+    before = dt.datetime.now()
+    time.sleep(1)
+    now = dt.datetime.now()
+    delta = now - before
+    seconds = delta.total_seconds()
+    print type(seconds)  # <type 'float'>
+    print '%s seconds' % seconds
+    print '%f seconds' % seconds
+
+
+def how_many_days_past_in_a_year():
+    some_day = dt.datetime.strptime('May 5 2007', '%B %d %Y').date()
+    assert isinstance(some_day, dt.date)
+    year = some_day.year
+    print type(year)  # <type 'int'>
+    first_day_of_a_year = '%s/01/01' % year
+    first_day_of_a_year = dt.datetime.strptime(first_day_of_a_year, '%Y/%m/%d').date()
+    day_delta = (some_day - first_day_of_a_year).days + 1
+    print some_day, first_day_of_a_year
+    print day_delta
+
+
+parse_format()
+#total_seconds()
+#how_many_days_past_in_a_year()
