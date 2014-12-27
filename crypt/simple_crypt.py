42beab27dec5824ed9f52e3344222e08d5dc9ad8
thinker3 <metathinkerk@gmail.com>
2014-12-27 16:39:07 +0800

    size after encyption

--- crypt/simple_crypt.py
+++ crypt/simple_crypt.py
@@ -32,6 +32,31 @@ def wrong():
     plaintext = decrypt(key, ciphertext)
     print plaintext
 
+
+def size(n):
+    plaintext = 'a' * n
+    key = 'python'
+    ciphertext = encrypt(key, plaintext)
+    print n, len(ciphertext)
+    plaintext = decrypt(key, ciphertext)
+    #print ciphertext
+    print '[%s]' % plaintext
+
+
+def test_size():
+    size(0)
+    size(1)
+    size(2)
+    size(8)
+    size(9)
+    size(10)
+    size(16)
+    size(17)
+    size(18)
+
+'''
 right()
 print
 wrong()
+'''
+test_size()
