6ec77fbb0789ecd09d5705804c1270d9cd83d4bd
thinker3 <metathinkerk@gmail.com>
2015-03-13 22:38:22 +0800

    keydown to keypress

--- splinter_js.py
+++ splinter_js.py
@@ -1,5 +1,6 @@
 from splinter.browser import Browser
-from time import sleep
+#from time import sleep
+
 b = Browser('firefox')
 b.visit('http://www.baidu.com')
 #b.fill('wd', 'test\r')
@@ -11,11 +12,10 @@ b.fill('wd', 'test')
 
 script = '''
     var e = document.createEvent('KeyboardEvent');
-    e.initKeyEvent('keydown', true, true, window, false, false, false, false, 13, 0);
-    document.getElementsByName('wd')[0].dispatchEvent(e)
+    e.initKeyEvent('keypress', true, true, window, false, false, false, false, 13, 0);
+    document.getElementsByName('wd')[0].dispatchEvent(e);
 '''
 b.execute_script(script)
 #sleep(5)
 b.type('wd', ' python')
 #b.type('wd', ' python', slowly=True) # slowly not working
-
