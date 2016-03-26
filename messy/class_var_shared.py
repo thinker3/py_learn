14e004e0035ee412a60a81adb846ffb5c7354494
thinker3 <penomivy@gmail.com>
2016-03-26 18:06:04 +0800

    class variable

---
+++ messy/class_var_shared.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class A(object):
+    # class variables are not shared among processes
+    numbers = []
+
+    def append(self, num):
+        self.numbers.append(num)
+
+
+a = A()
+a.append(1)
+print a.numbers
+b = A()
+print b.numbers
