7e0829cd5b4eb18bee184edee29ce1f0b551a157
thinker3 <penomivy@gmail.com>
2016-04-03 09:53:34 +0800

    Printer(threading.Thread)

--- thread/queue_ctrl_c.py
+++ thread/queue_ctrl_c.py
@@ -11,10 +11,6 @@ import threading
 lock = threading.Lock()
 worker_number = 3
 tasks = range(12)
-Queue.running = True
-q = Queue.Queue()
-for item in tasks:
-    q.put(item)
 
 
 def do_work(item):
@@ -24,19 +20,53 @@ def do_work(item):
     lock.release()
 
 
-def worker():
+def worker(q):
     while Queue.running:
         item = q.get()
         do_work(item)
         if q.empty():
             Queue.running = False
 
-for i in range(worker_number):
-     t = threading.Thread(target=worker)
-     t.start()
 
-try:
-    while Queue.running:
-        time.sleep(1)
-except KeyboardInterrupt:
-    Queue.running = False
+def main_of_func():
+    Queue.running = True
+    q = Queue.Queue()
+    [q.put(item) for item in tasks]
+    [threading.Thread(target=worker, args=(q, )).start() for i in range(worker_number)]
+    try:
+        while Queue.running:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        Queue.running = False
+
+
+class Printer(threading.Thread):
+    running = True
+
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while self.running:
+            if self.queue.empty():
+                self.running = False
+            task = self.queue.get()
+            lock.acquire()
+            time.sleep(1)
+            print(task)
+            lock.release()
+
+
+def main_of_class():
+    q = Queue.Queue()
+    [q.put(item) for item in tasks]
+    [Printer(q).start() for i in range(worker_number)]
+    try:
+        while Printer.running:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        Printer.running = False
+
+# main_of_func()
+main_of_class()
