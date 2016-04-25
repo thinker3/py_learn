53c508fc589bcaf73ff60d7d3ab5163d93a03e57
thinker3 <penomivy@gmail.com>
2016-04-25 18:16:57 +0800

    truck_num

--- regex/cn_re.py
+++ regex/cn_re.py
@@ -1,7 +1,7 @@
-#coding=utf8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import re
-
 
 a = u'不要家私的勿扰'
 aa = u'不需要家私的勿扰'
@@ -14,7 +14,6 @@ d = re.search(p, aaa, re.I).group()
 e = re.search(p, aaaa, re.I).group()
 print b, c, d, e
 
-
 print
 s = '毛织物 Woollen fabrics shrink in the wash. 缩水。a test'
 p = re.compile(r'\w+')  # english only
@@ -26,3 +25,10 @@ print word_list
 p = re.compile(r'\w{2,}')  # english only
 word_list = p.findall(s)
 print word_list
+
+pattern = re.compile(ur'^[\u4e00-\u9fa5][A-Za-z][A-Za-z0-9]{5}$')
+truck_num = u'渝A12345'
+print pattern.match(truck_num)
+print pattern.match('渝A12345')
+print pattern.match(u'渝A1234')
+print pattern.match(u'渝A123456')
