33eb54b4dc65da0f8542502ee434d63e96d2edbb
thinker3 <penomivy@gmail.com>
2016-08-09 04:47:29 +0800

    filter by attribute

--- messy/filter_test.py
+++ messy/filter_test.py
@@ -3,6 +3,7 @@ lit = filter(None, li)
 print li
 print lit
 
+
 def my_filter(item):
     if isinstance(item, str):
         return True
@@ -15,3 +16,21 @@ lit = filter(my_filter, li)
 print li
 print lit
 
+
+class Nonsense(object):
+
+    def __init__(self, sense):
+        self.sense = sense
+
+    def __repr__(self):
+        return repr(self.sense)
+
+li = [
+    Nonsense(None),
+    Nonsense(0),
+    Nonsense([]),
+    Nonsense(1),
+    Nonsense({}),
+    Nonsense('href'),
+]
+print filter(lambda x: x.sense, li)
