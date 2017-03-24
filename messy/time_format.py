66b1f47393c117b509a3c27a6b767eeec535a57e
thinker3 <penomivy@gmail.com>
2017-03-24 13:48:40 +0800

    parse time

--- messy/time_format.py
+++ messy/time_format.py
@@ -7,3 +7,13 @@ now = dt.datetime.now()
 time = now.time()
 print time.strftime('%H:%M')
 print time.strftime('%H:%M:%S')
+
+time_string = '20:13:14'
+# AttributeError: type object 'datetime.time' has no attribute 'strptime'
+# print dt.time.strptime(time_string, '%H:%M')
+# print dt.time.strptime(time_string, '%H:%M:%S')
+
+# ValueError: unconverted data remains: :14
+# print dt.datetime.strptime(time_string, '%H:%M').time()
+love = dt.datetime.strptime(time_string, '%H:%M:%S').time()
+print repr(love)
