b78300d0377957f82a7fefb6a9362f209f443634
thinker3 <penomivy@gmail.com>
2017-12-27 15:38:40 +0800

    ssh_rsa

---
+++ crypto/openssl_rsa_key.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rsa
+
+pub = open('./pub.pem', 'rb').read()
+
+pubkey = rsa.PublicKey.load_pkcs1(pub)
+__import__('ipdb').set_trace()
+print pubkey
---
+++ crypto/ssh_rsa.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rsa
+import keys
+
+secret = 'hello'
+pubkey = open('./pubkey.pem', 'rb').read()
+pubkey = rsa.PublicKey.load_pkcs1(pubkey)
+privkey = open('./privkey.pem', 'rb').read()
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
+
+encrypted = rsa.encrypt(secret, pubkey)
+decrypted = rsa.decrypt(encrypted, privkey)
+print decrypted, decrypted == secret
+
+secret = 'world'
+pubkey = rsa.PublicKey.load_pkcs1(keys.pubkey)
+encrypted = rsa.encrypt(secret, pubkey)
+privkey = rsa.PrivateKey.load_pkcs1(keys.privkey)
+decrypted = rsa.decrypt(encrypted, privkey)
+print decrypted, decrypted == secret
