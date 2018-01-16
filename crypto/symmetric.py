eb45a466cf599bf93530e3c8e64ea64196f913fa
thinker3 <penomivy@gmail.com>
2018-01-16 15:21:04 +0800

    def digest

--- crypto/symmetric.py
+++ crypto/symmetric.py
@@ -13,22 +13,26 @@ def unicode_to_utf8(s):
     return s
 
 
+def digest(key):
+    return hashlib.sha256(key).digest()
+
+
 class AESCipher(object):
     def __init__(self, key):
         self.bs = 16
-        self.key = hashlib.sha256(key.encode()).digest()
+        self.key = key
 
     def encrypt(self, message):
         message = self._pad(unicode_to_utf8(message))
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(message)).decode('utf-8')
+        return base64.b64encode(iv + cipher.encrypt(message))
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return self._unpad(cipher.decrypt(enc[AES.block_size:]))
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
