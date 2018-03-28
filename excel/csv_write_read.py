c912b4c1593cc6079b1cc9264de52d3db5a95bfd
thinker3 <penomivy@gmail.com>
2018-03-28 09:38:08 +0800

    UnicodeGBKReader

--- excel/csv_write_read.py
+++ excel/csv_write_read.py
@@ -5,24 +5,42 @@
 import codecs
 # The csv module doesn’t directly support reading and writing Unicode
 import unicodecsv as csv
+from utils.csv import UnicodeGBKReader
 
-with open('test.csv', "wb") as f:
-    f.writer(codecs.BOM_UTF8)
-    writer = csv.writer(f,
-            delimiter='\t', encoding='utf-8',
-            quotechar='"', quoting=csv.QUOTE_ALL)
-    rows = [
-        ['a', 'b', 'c'],
-        ['1', '2', '3'],
-        ['hello', ',', 'world'],
-    ]
-    for row in rows:
-        # row = ['="%s"' % one for one in row]
-        writer.writerow(row)
 
-with open('test.csv', "rb") as f:
-    reader = csv.reader(f,
-            delimiter='\t', encoding='utf-8',
-            quotechar='"', quoting=csv.QUOTE_ALL)
-    for row in reader:
-        print row
+csv.reader = UnicodeGBKReader
+
+
+def test():
+    with open('test.csv', "wb") as f:
+        f.writer(codecs.BOM_UTF8)
+        writer = csv.writer(f,
+                delimiter='\t', encoding='utf-8',
+                quotechar='"', quoting=csv.QUOTE_ALL)
+        rows = [
+            ['a', 'b', 'c'],
+            ['1', '2', '3'],
+            ['hello', ',', 'world'],
+        ]
+        for row in rows:
+            # row = ['="%s"' % one for one in row]
+            writer.writerow(row)
+
+    with open('test.csv', "rb") as f:
+        reader = csv.reader(f,
+                delimiter='\t', encoding='utf-8',
+                quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in reader:
+            print row
+
+
+def test_gbk():
+    with open('excel/gbk.csv', 'rb') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in reader:
+            print row
+
+
+if __name__ == '__main__':
+    test_gbk()
+    pass
