4cf249fbf1c5fdeee992dd47e7544b0fa7df5613
thinker3 <penomivy@gmail.com>
2018-05-17 11:35:50 +0800

    order_by

--- messy/list_sort.py
+++ messy/list_sort.py
@@ -1,30 +1,21 @@
-l = [1, 5, 3, 2, 7]
-print l
-print l.sort()
-print l
-
-l = [(2, 33.8, 40), (3, 43.15, 10), (4, 37.97, 16), (5, 46.81, 36),
-     (6, 48.77, 79), (8, 19.36, 79), (9, 6.76, 64)]
-print l
-l.sort(key=lambda x: x[1])
-print l
-l.sort(key=lambda x: x[2])
-print l
-
-l = [(2, 33.8, 40), (3, 43.15, 10), (4, 37.97, 16), (5, 46.81, 36),
-     (6, 48.77, 79), (8, 19.36, 79), (9, 6.76, 64)]
-l.sort(key=lambda x: x[2])
-print l
-l.sort(key=lambda x: x[1])
-print l
-
-l = [(2, 'ab'), (2, 'ac'), (1, 'ad'), (2, 'aa')]
-l.sort(key=lambda x: x[0])
-print l
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import operator
-l.sort(key=operator.itemgetter(0, 1))
-print l
+
+li = [1, 5, 3, 2, 7]
+assert li.sort() is None
+assert li == [1, 2, 3, 5, 7]
+
+li = [(3, 'ab'), (2, 'ac'), (1, 'ad'), (2, 'aa')]
+li.sort(key=lambda x: x[0])  # order_by
+assert li == [(1, 'ad'), (2, 'ac'), (2, 'aa'), (3, 'ab')]
+li.sort(key=lambda x: x[1])
+assert li == [(2, 'aa'), (3, 'ab'), (2, 'ac'), (1, 'ad')]
+li.sort(key=lambda x: x[1], reverse=True)  # order_by
+assert li == [(1, 'ad'), (2, 'ac'), (3, 'ab'), (2, 'aa')]
+li.sort(key=operator.itemgetter(0, 1))  # order_by
+assert li == [(1, 'ad'), (2, 'aa'), (2, 'ac'), (3, 'ab')]
 
 
 class Person(object):
@@ -33,14 +24,12 @@ class Person(object):
         self.name = name
         self.age = age
 
+
 people = [
     Person('Jim', 5),
     Person('Tom', 3),
     Person('Bob', 9),
 ]
-
-from operator import attrgetter
-
-people.sort(key=attrgetter('age'), reverse=True)
-for person in people:
-    print(person.name, person.age)
+people.sort(key=operator.attrgetter('age'), reverse=True)  # order_by
+ages = [person.age for person in people]
+assert ages == [9, 5, 3]
