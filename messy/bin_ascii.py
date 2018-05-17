060035160a85935db0c959e35039a4a81e67ac5f
thinker3 <penomivy@gmail.com>
2018-05-17 09:50:59 +0800

    binascii re-learned

--- messy/bin_ascii.py
+++ messy/bin_ascii.py
@@ -3,11 +3,23 @@
 
 import binascii
 
-a = binascii.b2a_hex('123abc')
-print a
-
+s = '\x01\xab\xcda1'
+a = binascii.b2a_hex(s)
+assert a == '01abcd6131'
 b = binascii.a2b_hex(a)
-print b
+assert b == s
 
-s = '\x01\xab\xcd'
-print map(binascii.hexlify, s)
+a = binascii.b2a_base64(s)
+assert a == 'AavNYTE=\n'
+b = binascii.a2b_base64(a)
+assert b == s
+
+a = binascii.hexlify(s)
+assert a == '01abcd6131'
+b = binascii.unhexlify(a)
+assert b == s
+
+r = repr(s)
+assert r == r"'\x01\xab\xcda1'"
+t = eval(r)  # todo
+assert s == t
